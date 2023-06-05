872c0366b1ed6d806363c6c7042ea37771e9ee48
Martin Iribas <martin.iribass@gmail.com>
2023-06-05 19:33:52 -0300

    ultimos cambios

--- src/components/sections/HomeFormSection.tsx
+++ src/components/sections/HomeFormSection.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import "../../index.css"
 import BackgroundImg from '../../assets/playagreed.png'
 import BackgroundWhite from '../../assets/withegrid.png'
 
@@ -10,16 +9,21 @@ interface Props {
 const HomeFormSection: React.FC<Props> = ({ children }) => {
   return (
     <div className="flex h-screen">
-      <section className="w-2/3 flex justify-center items-center relative z-10">
-        <div id='form' className="shadow-lg h-20" style={{ backgroundImage: `url(${BackgroundWhite})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          <h1>Sarar</h1>
+      <section className="w-2/3 flex justify-center items-center relative z-10"
+      style={{ 
+        backgroundImage: `url(${BackgroundWhite})`, 
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center' 
+        }}
+      >
+        <div id='form' className="shadow-lg" >
           {children}
         </div>
       </section>
       
-      <section className="w-1/3 relative z-0">
-        <div className="h-full bg-cover bg-center bg-no-repeat">
-          <img className="h-full bg-center" src={BackgroundImg} alt="Background Image" />
+      <section className="w-1/3 relative">
+        <div className="h-full w-full z-0 bg-cover bg-center bg-no-repeat">
+          <img className="h-full" src={BackgroundImg} alt="Background Image" />
         </div>
       </section>
     </div>

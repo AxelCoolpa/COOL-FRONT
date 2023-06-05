23b97b8fd6e0a8ef685bd6628e039e9d7043eda3
Martin Iribas <martin.iribass@gmail.com>
2023-06-05 19:13:07 -0300

    acomodando login y register

--- src/components/sections/HomeFormSection.tsx
+++ src/components/sections/HomeFormSection.tsx
@@ -10,12 +10,13 @@ interface Props {
 const HomeFormSection: React.FC<Props> = ({ children }) => {
   return (
     <div className="flex h-screen">
-      <section className="w-2/3 bg-gray-200 flex justify-center items-center relative z-10">
-        <div id='primero' className="max-w-lg p-8 bg-white shadow-lg rounded-md" style={{ backgroundImage: `url(${BackgroundWhite})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <section className="w-2/3 flex justify-center items-center relative z-10">
+        <div id='form' className="shadow-lg h-20" style={{ backgroundImage: `url(${BackgroundWhite})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
           <h1>Sarar</h1>
           {children}
         </div>
       </section>
+      
       <section className="w-1/3 relative z-0">
         <div className="h-full bg-cover bg-center bg-no-repeat">
           <img className="h-full bg-center" src={BackgroundImg} alt="Background Image" />
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import Login from "../components/Login";
+import HomeFormSection from "../components/sections/HomeFormSection";
+
+const LoginPage = () => {
+  return (
+        <HomeFormSection>
+            <Login />
+        </HomeFormSection>
+  )
+}
+
+export default LoginPage;

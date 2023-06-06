4727e804d32f15028d2509482040d6ce39dc6a77
Martin Iribas <martin.iribass@gmail.com>
2023-06-06 20:48:42 -0300

    controladores de rutas

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/Store.ts'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, RouterProvider } from 'react-router-dom'
+import { router } from './routes/routeControl.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
-    <Router>
+    {/* <Router>
       <App />
-    </Router>
+    </Router> */}
+    <RouterProvider router={router} />
   </Provider>,
 )

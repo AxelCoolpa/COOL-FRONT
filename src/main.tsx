abdd8277c06104d1db2768d0522be3ac762dfe51
Martin Iribas <martin.iribass@gmail.com>
2023-06-06 20:55:01 -0300

    routes latest

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/Store.ts'
@@ -8,9 +7,6 @@ import { router } from './routes/routeControl.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
-    {/* <Router>
-      <App />
-    </Router> */}
     <RouterProvider router={router} />
   </Provider>,
 )

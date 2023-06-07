bd3eea8235e7b070ee1ba3cd4bf115a33dff359b
MrTban <esteman214@gmail.com>
2023-06-07 15:01:06 -0500

    add types

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -6,14 +6,15 @@ interface InputProps {
 	required?: boolean
 }
 
-const Input: React.FC<InputProps> = ({ id, label, type, disabled }) => {
+const Input: React.FC<InputProps> = ({ id, label, type, disabled, required }) => {
 	return (
 		<div className='w-full relative'>
 			<input
 				id={id}
-				disabled={disabled}
 				placeholder={label}
 				type={type}
+				disabled={disabled}
+				required={required}
 				className='border border-[#E8ECF4] bg-[#F7F8F9] p-2 w-[144px] h-[49px] rounded-lg  outline-none transition disabled:opacity-70 disabled:cursor-not-allowed focus:border-black'
 			/>
 		</div>

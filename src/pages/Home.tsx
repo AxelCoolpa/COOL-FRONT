47a53b0e25e4b4998017eb6901c317097e25b23d
MrTban <esteman214@gmail.com>
2023-06-07 10:31:47 -0500

    render HeaderSection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SideBar from '../components/SideBar'
+import HeaderSection from '../components/sections/HeaderSection'
 
 const Home = () => {
-  return (
-    <div>
-      <SideBar/>
-    </div>
-  )
+	return (
+		<div>
+			<SideBar />
+			<HeaderSection />
+		</div>
+	)
 }
 
 export default Home

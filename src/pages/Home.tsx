f97770d1b4a7c6a55f195c6facac69140331e518
MrTban <esteman214@gmail.com>
2023-06-07 16:35:58 -0500

    esqueleton

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,9 @@ const Home = () => {
 	return (
 		<div>
 			<SideBar />
-			<div className='flex flex-col items-end xl:items-center w-full p-5 md:pl-[220px] xl:pl-[270px] gap-20'>
+			<div className='flex flex-col items-end xl:items-center w-full p-5 md:pl-[220px] xl:pl-[270px] gap-24'>
 				<HeaderSection />
-				<div className='w-full bg-black h-[200px]'></div>
+				<div className='bg-[#E8ECF4] w-full h-screen rounded-2xl'></div>
 			</div>
 		</div>
 	)

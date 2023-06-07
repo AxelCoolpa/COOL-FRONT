fd83e79db5c2d23272f9ccfc7af1146ff10060b5
SadiLuis <84958501+SadiLuis@users.noreply.github.com>
2023-06-07 15:54:19 -0300

    Configuracion de Test modificada. ahora lee la prueba pero Typescript no la pasa

--- src/test/login.test.ts
+++ src/test/login.test.ts
@@ -1,9 +1,35 @@
-import { render, RenderResult } from '@testing-library/react';
-import Login from '../components/Login';
+import { render, fireEvent, screen } from "@testing-library/react";
+import Login from "../components/Login";
 
-test('renders Login component correctly', () => {
-    const { getByText, getByPlaceholderText }: RenderResult = render(<Login />);
-
+test("renders Login component", () => {
+  render(<Login />);
+  
   // Verificar que el componente se renderiza correctamente
-  getByText('Hello!');
+  expect(screen.getByText("Hello!")).toBeInTheDocument();
+  expect(screen.getByText("Login to get started!")).toBeInTheDocument();
+  expect(screen.getByPlaceholderText("Username")).toBeInTheDocument();
+  expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: "Login" })).toBeInTheDocument();
+});
+
+test("handles form submission", () => {
+  render(<Login />);
+  
+  // Simular la entrada de datos en los campos de formulario
+  fireEvent.change(screen.getByPlaceholderText("Username"), {
+    target: { value: "testuser" },
+  });
+  
+  fireEvent.change(screen.getByPlaceholderText("Password"), {
+    target: { value: "testpassword" },
+  });
+  
+  // Simular la presentación del formulario
+  fireEvent.click(screen.getByRole("button", { name: "Login" }));
+  
+  // Verificar que los datos del formulario se imprimen en la consola
+  expect(console.log).toHaveBeenCalledWith({
+    username: "testuser",
+    password: "testpassword",
+  });
 });

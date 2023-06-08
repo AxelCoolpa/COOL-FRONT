2915274c8f3950c855993a4143aba74baecefc03
MrTban <esteman214@gmail.com>
2023-06-08 13:30:25 -0500

    change image property type and add galleryPhoto

--- src/mocks/listingsCards.ts
+++ src/mocks/listingsCards.ts
@@ -3,8 +3,10 @@ export const listings = [
 		id: '1',
 		name: 'Canal of panama',
 		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
-		imageSrc:
+		imageSrc: [
+			// Agregar 3 imagenes una principal para mostar en el header y las otras 2 para que vayan rotando en el detail de cada destino
 			'https://res.cloudinary.com/dhfrzje8b/image/upload/v1685733561/vbff7ck5tzvssegdzuga.jpg',
+		],
 		categories: ['ciudad'],
 		locationValue: 'PA',
 		usserId: '1',
@@ -14,13 +16,19 @@ export const listings = [
 		endTime: '03:00',
 		rate: '5',
 		price: '70',
+		galleryPhoto: [
+			// Aqui arear imagenes para mostrar en la aleria de los paquetes
+			'',
+		],
 	},
 	{
 		id: '2',
 		name: 'Cancún',
 		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
-		imageSrc:
+		imageSrc: [
+			// Agregar 3 imagenes una principal para mostar en el header y las otras 2 para que vayan rotando en el detail de cada destino
 			'https://res.cloudinary.com/dhfrzje8b/image/upload/v1685711691/l4anbxuvkuvsgbakya0u.webp',
+		],
 		categories: ['playa'],
 		locationValue: 'MX',
 		usserId: '1',
@@ -30,13 +38,19 @@ export const listings = [
 		endTime: '18:00',
 		rate: '4.6',
 		price: '150',
+		galleryPhoto: [
+			// Aqui arear imagenes para mostrar en la aleria de los paquetes
+			'',
+		],
 	},
 	{
 		id: '3',
 		name: 'Machu Picchu',
 		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
-		imageSrc:
+		imageSrc: [
+			// Agregar 3 imagenes una principal para mostar en el header y las otras 2 para que vayan rotando en el detail de cada destino
 			'https://res.cloudinary.com/dhfrzje8b/image/upload/v1685734732/oymu6yvrhbvcntu51thj.jpg',
+		],
 		categories: ['montaña'],
 		locationValue: 'PE',
 		usserId: '1',
@@ -46,13 +60,19 @@ export const listings = [
 		endTime: '17:00',
 		rate: '4.7',
 		price: '120',
+		galleryPhoto: [
+			// Aqui arear imagenes para mostrar en la aleria de los paquetes
+			'',
+		],
 	},
 	{
 		id: '4',
 		name: 'Dubai',
 		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
-		imageSrc:
+		imageSrc: [
+			// Agregar 3 imagenes una principal para mostar en el header y las otras 2 para que vayan rotando en el detail de cada destino
 			'https://res.cloudinary.com/dhfrzje8b/image/upload/v1685734410/aundzma8lmkdxexavem6.webp',
+		],
 		categories: ['desierto'],
 		locationValue: 'AE',
 		usserId: '1',
@@ -62,5 +82,9 @@ export const listings = [
 		endTime: '13:00',
 		rate: '4.5',
 		price: '170',
+		galleryPhoto: [
+			// Aqui arear imagenes para mostrar en la aleria de los paquetes
+			'',
+		],
 	},
 ]

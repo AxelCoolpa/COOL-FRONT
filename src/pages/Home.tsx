5328b1a17dd09d96894480f3e28ae3c367839664
MrTban <esteman214@gmail.com>
2023-06-08 11:35:36 -0500

    render ListingCard mapping

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,31 @@
 import React from 'react'
+
+import { listings } from '../mocks/listingsCards'
+
 import SideBar from '../components/sidebar/SideBar'
 import HeaderSection from '../components/sections/HeaderSection'
+import ListingCard from '../components/listings/ListingCard'
+
 import headerImg from '../assets/headerImg.jpg'
 
 const Home = () => {
 	return (
 		<div>
 			<SideBar />
-			<div className='flex flex-col items-end xl:items-center w-full p-5 md:pl-[220px] xl:pl-[270px] gap-36'>
+			<div className='flex flex-col w-full p-5 md:pl-[220px] xl:pl-[270px] gap-36'>
 				<HeaderSection
 					title='Where do you want to go ?'
 					subtitle='Explore the best destinations in the world'
 					image={headerImg}
 				/>
-				<div className='bg-[#E8ECF4] w-full h-screen rounded-2xl'></div>
+				<div className='p-5'>
+					<h2 className='text-2xl font-semibold'>Trending adventure</h2>
+					<div className='pt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8'>
+						{listings.map((listing) => {
+							return <ListingCard key={listing.id} data={listing} />
+						})}
+					</div>
+				</div>
 			</div>
 		</div>
 	)

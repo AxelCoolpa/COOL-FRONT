0bdec4ae187d0e9e8f2d4586c55243e5c9c3dbf3
MrTban <esteman214@gmail.com>
2023-06-08 13:00:52 -0500

    Card partially completed

--- src/components/listings/ListingCard.tsx
+++ src/components/listings/ListingCard.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { AiFillStar } from 'react-icons/ai'
+import { BsDot } from 'react-icons/bs'
 
 interface EnumData {
 	id: string
@@ -8,10 +10,12 @@ interface EnumData {
 	categories: Array<string>
 	locationValue: string
 	usserId: string
-	activityes: Array<string>
+	activities: Array<string>
 	starterPack: string
 	startTime: string
 	endTime: string
+	rate: string
+	price: string
 }
 
 interface ListingCardProps {
@@ -22,9 +26,39 @@ const ListingCard: React.FC<ListingCardProps> = ({ data }) => {
 	return (
 		<div
 			onClick={() => alert(`/listings/${data.id}`)}
-			className='col-span-1 cursor-pointer group'
+			className='bg-[#f7f8f9] col-span-1 cursor-pointer group rounded-xl pb-4'
 		>
-			ListingCard
+			<img src={data.imageSrc} className='object-cover w-full h-[200px] rounded-t-xl' />
+			<div className='flex items-center justify-between px-3 pt-4'>
+				<h4 className='text-base font-semibold'>{data.name}</h4>
+				<div className='flex items-center gap-1'>
+					<AiFillStar size={18} color='#faaf00' />
+					<span className='text-sm font-medium'>{data.rate}</span>
+				</div>
+			</div>
+			<ul className='flex flex-col justify-center px-3 py-4 text-xs gap-1 text-[#00000080]'>
+				<li>
+					<div className='flex items-center gap-1'>
+						<BsDot size={20} />
+						{data.activities}
+					</div>
+				</li>
+				<li>
+					<div className='flex items-center gap-1'>
+						<BsDot size={20} />
+						{data.starterPack}
+					</div>
+				</li>
+				<li>
+					<div className='flex items-center gap-1'>
+						<BsDot size={20} />
+						{data.startTime} to {data.endTime}
+					</div>
+				</li>
+			</ul>
+			<div className='flex justify-center text-white font-bold text-lg'>
+				<button className='bg-[#109460] w-full mx-4 rounded-lg'>{data.price}$</button>
+			</div>
 		</div>
 	)
 }

3f012030b3eeff33105e572f8589dbc2721bcd1c
MrTban <esteman214@gmail.com>
2023-06-08 11:33:30 -0500

    destinations mock created

---
+++ src/mocks/listingsCards.ts
@@ -0,0 +1,58 @@
+export const listings = [
+	{
+		id: '1',
+		name: 'Canal of panama',
+		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+		imageSrc:
+			'https://res.cloudinary.com/dhfrzje8b/image/upload/v1685733561/vbff7ck5tzvssegdzuga.jpg',
+		categories: ['ciudad'],
+		locationValue: 'PA',
+		usserId: '1',
+		activityes: ['4 disco tour'],
+		starterPack: '1 starter drink',
+		startTime: '20:00',
+		endTime: '03:00',
+	},
+	{
+		id: '2',
+		name: 'Cancún',
+		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+		imageSrc:
+			'https://res.cloudinary.com/dhfrzje8b/image/upload/v1685711691/l4anbxuvkuvsgbakya0u.webp',
+		categories: ['playa'],
+		locationValue: 'MX',
+		usserId: '1',
+		activityes: ['Diving'],
+		starterPack: 'A pair of fins',
+		startTime: '14:00',
+		endTime: '18:00',
+	},
+	{
+		id: '3',
+		name: 'Machu Picchu',
+		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+		imageSrc:
+			'https://res.cloudinary.com/dhfrzje8b/image/upload/v1685734732/oymu6yvrhbvcntu51thj.jpg',
+		categories: ['montaña'],
+		locationValue: 'PE',
+		usserId: '1',
+		activityes: ['Hiking'],
+		starterPack: 'A map and bottle of water',
+		startTime: '09:00',
+		endTime: '17:00',
+	},
+	{
+		id: '4',
+		name: 'Dubai',
+		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+		imageSrc:
+			'https://res.cloudinary.com/dhfrzje8b/image/upload/v1685734410/aundzma8lmkdxexavem6.webp',
+		categories: ['desierto'],
+		locationValue: 'AE',
+		usserId: '1',
+		activityes: ['Sandboarding'],
+		starterPack: '1 sandboard',
+		startTime: '08:00',
+		endTime: '13:00',
+	},
+]

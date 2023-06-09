311e89860a78aae5f11163acb6aeed7f7cadc7b7
MrTban <esteman214@gmail.com>
2023-06-09 16:47:15 -0500

    create EnumData interface

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export interface EnumData {
+	id: string
+	name: string
+	description: string
+	imageSrc: Array<string>
+	categories: Array<string>
+	locationValue: string
+	usserId: string
+	activities: Array<string>
+	starterPack: string
+	startTime: string
+	endTime: string
+	rating: Array<number>
+	price: string
+}

2f56f368dbc1f01f0fafedd4ba1316112c0d0e7c
Martin Iribas <martin.iribass@gmail.com>
2023-06-09 14:08:39 -0300

    strore_slice

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import registerReducer from "../features/RegisterSlice";
 import counterSlice from "../features/CreatedSlice"
 import LoginSlice from "../features/LoginSlice";
+/* import authSlice from "../features/authSlice"; */
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
     register: registerReducer,
-    login: LoginSlice
+    login: LoginSlice,
+    /* auth: authSlice, */
   },
 });
 

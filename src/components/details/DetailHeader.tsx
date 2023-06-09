0ebb53f7e11c59acc10a7d251313ce79de98a35b
MrTban <esteman214@gmail.com>
2023-06-09 15:31:29 -0500

    grabbing the images of each object

--- src/components/details/DetailHeader.tsx
+++ src/components/details/DetailHeader.tsx
@@ -1,15 +1,33 @@
 import React from 'react'
 
-import headerImg from '../../assets/headerImg.jpg'
+interface EnumListing {
+	imageSrc: Array<string>
+}
+interface DetailHeaderProps {
+	listing: EnumListing
+}
 
-const DetailHeader = () => {
+const DetailHeader: React.FC<DetailHeaderProps> = ({ listing }) => {
+	console.log(listing)
+
 	return (
-		<div className='w-full max-h-[400px] bg-black'>
+		<div className='w-full max-h-[400px]'>
 			<div className='flex gap-5'>
-				<img src={headerImg} className='object-cover w-full h-[400px] rounded-[20px]' />
+				<img
+					src={listing.imageSrc[0]}
+					className='object-cover w-full h-[400px] rounded-[20px]'
+				/>
 				<div className='flex flex-col object-cover gap-5'>
-					<img src={headerImg} alt='' className='object-cover h-[190px] rounded-[20px]' />
-					<img src={headerImg} alt='' className='object-cover h-[190px] rounded-[20px]' />
+					<img
+						src={listing?.imageSrc[1]}
+						alt=''
+						className='object-cover h-[190px] rounded-[20px]'
+					/>
+					<img
+						src={listing?.imageSrc[2]}
+						alt=''
+						className='object-cover h-[190px] rounded-[20px]'
+					/>
 				</div>
 			</div>
 		</div>

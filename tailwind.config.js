c66456a8fe135cf6505f1e3c964f61f853b14578
MrTban <esteman214@gmail.com>
2023-06-10 13:21:11 -0500

    create box shadow property CooL

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				CooL: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+			},
+		},
 	},
 	plugins: [],
 }

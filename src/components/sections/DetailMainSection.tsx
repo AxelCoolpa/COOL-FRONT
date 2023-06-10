f652ace6babc571540e8e85d435addace0e257c1
MrTban <esteman214@gmail.com>
2023-06-10 13:23:12 -0500

    add detailIndo & bookingCard

--- src/components/sections/DetailMainSection.tsx
+++ src/components/sections/DetailMainSection.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-import DetailHeader from '../details/DetailHeader'
 import { listings } from '../../mocks/listingsCards'
+
+import DetailHeader from '../details/DetailHeader'
+import DetailInfo from '../details/DetailInfo'
+import BookingCard from '../details/BookingCard'
 
 const DetailMainSection = () => {
 	const { id } = useParams()
 
 	const listing = listings.find((item) => item.id === id)
 
-	return <DetailHeader listing={listing} />
+	return (
+		<div className='max-w-screen-2xl mx-auto'>
+			<DetailHeader listing={listing} />
+			<div className='grid grid-cols-1 lg:grid-cols-7 md:gap-10 pt-16'>
+				<DetailInfo
+					name={listing?.name}
+					rating={listing?.rating}
+					description={listing?.description}
+				/>
+				<div className='order-first mb-10 lg:order-last lg:col-span-3'>
+					<BookingCard price={listing?.price} />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default DetailMainSection

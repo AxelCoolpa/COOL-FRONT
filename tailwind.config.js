1c1fb50ede5057e7c4ab218908ad447fb86d4b73
MrTban <esteman214@gmail.com>
2023-06-12 14:24:36 -0500

    create button color prop

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
+			colors: {
+				BtnCooL: '#ce452a',
+			},
 			boxShadow: {
 				CooL: '0px 0px 10px rgba(0, 0, 0, 0.1)',
 			},

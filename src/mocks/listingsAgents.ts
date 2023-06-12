4fd97d9f88a08539659a33cadc8b71745cea713d
MrTban <esteman214@gmail.com>
2023-06-12 10:43:47 -0500

    create mocks (user & agent)

---
+++ src/mocks/listingsAgents.ts
@@ -0,0 +1,9 @@
+import avatar from '../assets/Avatar.jpg'
+
+export const agent1 = {
+	userName: 'shapamel',
+	name: 'Shadom ',
+	lastName: 'Pamel',
+	mail: 'shadom.pamel@example.com',
+	avatar: avatar,
+}
---
+++ src/mocks/listingsUser.ts
@@ -0,0 +1,9 @@
+import avatar from '../assets/background-Cool.jpeg'
+
+export const user1 = {
+	userName: 'john03',
+	name: 'John',
+	lastName: 'Doe',
+	mail: 'john.doe@example.com',
+	avatar: avatar,
+}

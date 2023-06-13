f3a24d436abfae4cb77b38bf477145198f675df6
MrTban <esteman214@gmail.com>
2023-06-13 17:18:06 -0500

    DEFINIR TYPE

--- src/mocks/listingAdventure.ts
+++ src/mocks/listingAdventure.ts
@@ -4,22 +4,22 @@ export type listinAdventure = {
 	description: string
 	gallery: Array<string>
 	individualPrice: number
-	groupPrice?: number | Array<number>
-	categories: string | Array<string>
+	groupPrice?: number | Array<number> //? DEFINIR TYPE
+	categories: Array<string>
 	location: string
 
 	extras?: [
-		activities?: Array<string>,
-		starterPack?: Array<string>,
+		activities?: string | Array<string>, //? DEFINIR TYPE
+		starterPack?: string | Array<string>, //? DEFINIR TYPE
 		startTime?: string,
 		endTime?: string
 	]
 
 	rating?: Array<string>
-	reviewers?: Array<string>
+	reviews?: Array<string>
 
 	//! relaciones
 	providerId: string
 }
 
-//* Agregar propiedad para mostrar los mas comprados de cada proveedor
+//* Agregar propiedad para mostrar los más comprados de cada proveedor

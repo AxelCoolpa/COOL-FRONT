43700f0ae0893efff08500f7c0fa290f722e883c
MrTban <esteman214@gmail.com>
2023-06-13 10:59:10 -0500

    mock provider

---
+++ src/mocks/listingsProvider.ts
@@ -0,0 +1,12 @@
+import avatar from '../assets/background-Cool.jpeg'
+
+export const user1 = {
+	id: 'GS-2234',
+	userName: 'john03',
+	name: 'John',
+	lastName: 'Doe',
+	mail: 'john.doe@example.com',
+	phone: '+12 3456 678',
+	avatar: avatar,
+	address: 'New Avenue Street Corner South London 224151',
+}

9eb3ff2587c8152d00a5ef1312dcf919c3611fae
MrTban <esteman214@gmail.com>
2023-06-14 10:45:57 -0500

    import Map

--- src/components/sections/DetailMainSection.tsx
+++ src/components/sections/DetailMainSection.tsx
@@ -8,6 +8,7 @@ import DetailInfo from '../details/DetailInfo'
 import BookingCard from '../details/BookingCard'
 import ListenAgent from '../details/ListenAgent'
 import Gallery from '../details/Gallery'
+import Map from '../Map'
 
 const DetailMainSection = () => {
 	const { id } = useParams()
@@ -43,15 +44,7 @@ const DetailMainSection = () => {
 			{listing?.gallery > 2 && <hr className='w-full xl:w-4/6 2xl:w-3/6 mx-2 m-10' />}
 			<div className='flex flex-col py-10 gap-10'>
 				<h3 className='text-3xl font-bold'>Maps</h3>
-				<iframe
-					src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17090799.40050517!2d-82.28714719430437!3d8.55800738490379!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa61583c8be2be3%3A0x79eee04d1fa59bcf!2zUGFuYW3DoQ!5e0!3m2!1ses!2sco!4v1686660412555!5m2!1ses!2sco'
-					width='1000'
-					height='600'
-					style={{ border: 0, borderRadius: 20 }}
-					// allowfullscreen=''
-					loading='lazy'
-					// referrerpolicy='no-referrer-when-downgrade'
-				></iframe>
+				<Map />
 			</div>
 		</div>
 	)

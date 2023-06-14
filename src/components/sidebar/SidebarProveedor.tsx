7dabede705d1acfa316e065d05318f5c85d75c3c
Martin Iribas <martin.iribass@gmail.com>
2023-06-14 17:06:37 -0300

    sidebar customizado

--- src/components/sidebar/SidebarProveedor.tsx
+++ src/components/sidebar/SidebarProveedor.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { BiMenu } from 'react-icons/bi'
+import styles from '../../styles/Global'
 
 import Cool from '../../assets/cool.png'
 import Dropdown from "../dropdown/Dropdown";
@@ -14,20 +15,20 @@ import { FaBed, FaMapMarkerAlt } from "react-icons/fa";
 import { BsTicketPerforated } from "react-icons/bs";
 import { GiWavyItinerary } from "react-icons/gi";
 
-export default function Sidebar() {
+export default function SidebarProveedor() {
   const [collapseShow, setCollapseShow] = React.useState("hidden");
   return (
     <>
-      <nav className="md:left-3 md:block md:fixed md:top-3 md:bottom-3 md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-12 z-10 ">
-        <div className="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
+      <nav className={`${styles.nav}`}>
+        <div className="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap justify-between w-full mx-auto">
           {/* Toggler */}
           <button
             className="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
             type="button"
             onClick={() => setCollapseShow("bg-white m-2 py-3 px-6")}
-          >
+          > 
             <BiMenu />
-          
+
           </button>
           {/* Brand */}
           {/* <Link
@@ -40,6 +41,9 @@ export default function Sidebar() {
           </Link> */}
 
           {/* User */}
+
+
+
           <ul className="md:hidden items-center flex flex-wrap list-none">
             <li className="inline-block relative">
               <Dropdown />
@@ -48,6 +52,9 @@ export default function Sidebar() {
               <UserDropdown />
             </li>
           </ul>
+
+
+
 
           {/* Collapse */}
           <div
@@ -67,16 +74,20 @@ export default function Sidebar() {
                   </Link>
                 </div>
               </div>
-                <div className="w-6/12 flex relative">
-                  <button
-                    type="button"
-                    className="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
-                    onClick={() => setCollapseShow("hidden")}
-                  >
-                                    <BiMenu />
-                  </button>
-                </div>
+              <div className="w-6/12 flex relative">
+                <button
+                  type="button"
+                  className="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
+                  onClick={() => setCollapseShow("hidden")}
+                >
+                  <BiMenu />
+                </button>
+              </div>
             </div>
+
+
+
+
 
             {/* Form */}
             <form className="mt-6 mb-4 md:hidden">
@@ -88,72 +99,76 @@ export default function Sidebar() {
                 />
               </div>
             </form>
+
+
+
+
+
             {/* Divider */}
-
             <hr className="my-4 md:min-w-full" />
+
+
+
+
+
+
+
+
+
             {/* Heading */}
-            
+            <nav className='flex-grow ml-0 xl:ml-0 sm:mt-4 md:mt-0'>
+              <ul className='space-y-2'>
+                <li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6  gap-2 text-[#808080] hover:text-[#ce452a]'>
+                  <a href='/' className='font-semibold'>
+                    <GoHome />
+                      
+                  </a>
+                  <p className="flex-grow ml-3 xl:ml-3">   Dashboard</p>
+                </li>
+                <li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6  gap-2 text-[#808080] hover:text-[#ce452a]'>
+                  <a href='/adventure' className='font-semibold'>
+                    <GrSafariOption />
+                  </a>
+                  <p className="flex-grow ml-3 xl:ml-3">   Encuentra</p>
+                </li>
+                <li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6 gap-2 text-[#808080] hover:text-[#ce452a] '>
+                  <a href='/package' className='font-semibold'>
+                    <CgInfinity />
+                  </a>
+                  <p className="flex-grow ml-3 xl:ml-3">   Crear</p>
+                </li>
+                <li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6  gap-2 text-[#808080] hover:text-[#ce452a] '>
+                  <a href='/transport' className='font-semibold'>
+                    <RiCarFill />
+                  </a>
+                  <p className="flex-grow ml-3 xl:ml-3">transporte</p>
+                </li>
+                <li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6 gap-2 text-[#808080] hover:text-[#ce452a] '>
+                  <a href='/accommodation' className='font-semibold'>
+                    <FaBed />
+                  </a>  
+                  <p className="flex-grow ml-3 xl:ml-3">   Hoteles</p>
+                </li>
+                <li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6 gap-2 text-[#808080] hover:text-[#ce452a] '>
+                  <a href='/tickets' className='font-semibold'>
+                    <BsTicketPerforated />
 
+                  </a>
+                </li>
+                <li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6 gap-2 text-[#808080] hover:text-[#ce452a] '>
+                  <a href='/maps' className='font-semibold'>
+                    <FaMapMarkerAlt />
+                  </a>
 
+                </li>
+                <li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6 gap-2 text-[#808080] hover:text-[#ce452a] '>
+                  <a href='/itinarary' className='font-semibold'>
+                    <GiWavyItinerary />
+                  </a>
 
-
-
-
-
-
-
-            <nav className='flex-grow ml-2 xl:ml-4 sm:mt-4 md:mt-0'>
-					<ul className='space-y-2'>
-						<li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6  gap-2 text-[#808080] hover:text-[#ce452a]'>
-							<a href='/' className='font-semibold'>
-							<GoHome />
-								
-							</a>
-						</li>
-						<li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6  gap-2 text-[#808080] hover:text-[#ce452a]'>
-							<a href='/adventure' className='font-semibold'>
-							<GrSafariOption />
-								
-							</a>
-						</li>
-						<li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6 gap-2 text-[#808080] hover:text-[#ce452a] '>
-							<a href='/package' className='font-semibold'>
-							<CgInfinity />
-								
-							</a>
-						</li>
-						<li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6  gap-2 text-[#808080] hover:text-[#ce452a] '>
-							<a href='/transport' className='font-semibold'>
-							<RiCarFill />
-							</a>
-								
-						</li>
-						<li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6 gap-2 text-[#808080] hover:text-[#ce452a] '>
-							<a href='/accommodation' className='font-semibold'>
-							<FaBed />
-							</a>
-								
-						</li>
-						<li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6 gap-2 text-[#808080] hover:text-[#ce452a] '>
-							<a href='/tickets' className='font-semibold'>
-							<BsTicketPerforated />
-								
-							</a>
-						</li>
-						<li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6 gap-2 text-[#808080] hover:text-[#ce452a] '>
-							<a href='/maps' className='font-semibold'>
-							<FaMapMarkerAlt />
-							</a>
-								
-						</li>
-						<li className='flex items-center w-fit pt-4 pl-4 2xl:pt-6 gap-2 text-[#808080] hover:text-[#ce452a] '>
-							<a href='/itinarary' className='font-semibold'>
-							<GiWavyItinerary />
-							</a>
-								
-						</li>
-					</ul>
-				</nav>
+                </li>
+              </ul>
+            </nav>
 
 
             {/* <ul className="md:flex-col md:min-w-full flex flex-col list-none">
@@ -248,7 +263,7 @@ export default function Sidebar() {
             </ul> */}
 
             {/* Divider */}
-            <hr className="my-4 md:min-w-full" />          
+            <hr className="my-4 md:min-w-full" />
           </div>
 
         </div>
--- src/layouts/ProvDashboard.tsx
+++ src/layouts/ProvDashboard.tsx
@@ -4,7 +4,7 @@
 
 
 import SidebarProv from '../components/sidebar/sidebarProv'
-import SidebarB from '../components/sidebar/SidebarB'
+import SidebarB from '../components/sidebar/SidebarProveedor'
 import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
 import DashContainer from '../components/sections/dashContainer'
 import Navbar from '../components/Navbars/AdminNavbar'

bd830b864973e3d77df0d9d71b95c5fd0038ddbb
MrTban <esteman214@gmail.com>
2023-06-15 10:16:16 -0500

    add function average rating

--- src/components/sections/HeaderSection.tsx
+++ src/components/sections/HeaderSection.tsx
@@ -9,7 +9,7 @@ interface HeaderSectioonProps {
 	title?: string
 	subtitle?: string
 	name?: string
-	rate?: number
+	rate?: Array<string>
 	image?: string
 	favorite?: boolean
 	price?: number
@@ -24,6 +24,15 @@ const HeaderSection: React.FC<HeaderSectioonProps> = ({
 	favorite,
 	price,
 }) => {
+	const averageRating = rate
+		.reduce((a, b) => a + b / rate.length, 0)
+		.toString()
+		.slice(0, 3)
+
+	console.log(rate)
+
+	console.log(averageRating)
+
 	return (
 		<div className='relative flex items-center justify-center max-h-[475px] transition'>
 			<img
@@ -43,7 +52,7 @@ const HeaderSection: React.FC<HeaderSectioonProps> = ({
 					<div className='flex items-center'>
 						<Rating
 							name='text-feedback'
-							value={rate}
+							value={Number(averageRating)}
 							readOnly
 							precision={0.5}
 							emptyIcon={<AiFillStar style={{ opacity: 0.55 }} fontSize='inherit' />}

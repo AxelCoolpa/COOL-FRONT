04e577cc6acb1ae2a2df8815f191bf763ddff559
MrTban <esteman214@gmail.com>
2023-06-15 13:50:32 -0500

    top rated map

--- src/components/sections/HomeMainSection.tsx
+++ src/components/sections/HomeMainSection.tsx
@@ -5,6 +5,14 @@ import ListingCard from '../listings/ListingCard'
 import headerImg from '../../assets/headerImg.jpg'
 
 const HomeMainSection = () => {
+	const filterRating = listings?.filter(
+		(item) =>
+			item.rating
+				.reduce((a, b) => a + b / item.rating.length, 0)
+				.toString()
+				.slice(0, 3) >= '4.5'
+	)
+
 	return (
 		<div className='flex flex-col w-full gap-12 xl:gap-28 transition'>
 			<HeaderSection
@@ -15,7 +23,7 @@ const HomeMainSection = () => {
 			<div className='px-5'>
 				<h2 className='text-2xl font-semibold'>Trending adventure</h2>
 				<div className='pt-10 px-8 grid grid-cols-1 min-[950px]:grid-cols-2 min-[1200px]:grid-cols-3 min-[1440px]:grid-cols-4 gap-11'>
-					{listings.map((listing) => {
+					{filterRating.map((listing) => {
 						return <ListingCard key={listing.id} data={listing} />
 					})}
 				</div>

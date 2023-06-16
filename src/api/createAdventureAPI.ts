a5203f13798fc95555d31bbf2a3c84e25d50e533
MrTban <esteman214@gmail.com>
2023-06-16 13:21:57 -0500

    error (destinations to destination)

--- src/api/createAdventureAPI.ts
+++ src/api/createAdventureAPI.ts
@@ -7,7 +7,7 @@ export const ceateAdventureAPI = async (formData: typeof ceateAdventureFormData)
 	const user = await getUsersAPI()
 	const userID = user[1]._id
 
-	const URL = `http://localhost:3000/api/destinations/${userID}/create`
+	const URL = `http://localhost:3000/api/destination/${userID}/create`
 
 	try {
 		const response = await axios.post(URL, formData)

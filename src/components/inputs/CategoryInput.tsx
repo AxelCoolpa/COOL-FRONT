6b445b5de32422491f18b70cb04eadc6ad5f8021
MrTban <esteman214@gmail.com>
2023-06-17 14:58:42 -0500

    pequeños cambios

--- src/components/inputs/CategoryInput.tsx
+++ src/components/inputs/CategoryInput.tsx
@@ -3,14 +3,40 @@ import { IconType } from 'react-icons'
 interface CategoryInputProps {
 	icon: IconType
 	label: string
-	onClick: () => void
+	selected: boolean
+	onSelect: (value: string) => void
+	handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const CategoryInput: React.FC<CategoryInputProps> = ({ icon: Icon, label, onClick }) => {
+const CategoryInput: React.FC<CategoryInputProps> = ({
+	icon: Icon,
+	label,
+	selected,
+	onSelect,
+	handleChange,
+}) => {
 	return (
 		<div
-			onClick={() => onClick()}
-			className='flex flex-col items-center justify-center rounded-[20px] border border-[#F3F3F3] w-44 h-44 gap-3 hover:border-[#28B446] hover:border-4 transition cursor-pointer'
+			onClick={() => onSelect(label)}
+			onChange={handleChange}
+			className={`
+        flex
+        flex-col
+        items-center
+				justify-center
+        rounded-[20px]
+        gap-3
+				w-44
+				h-44
+        border
+        hover:border-[#28B446]
+        hover:border-4
+        transition
+        cursor-pointer        
+        ${selected ? 'border-[#28B446]' : 'border-[#F3F3F3]'}
+        ${selected ? 'border-4' : 'border'}
+        ${selected ? 'shadow-xl shadow-black/20' : 'shadow-none'}
+      `}
 		>
 			<Icon size={36} />
 			<div className='font-semibold'>{label}</div>

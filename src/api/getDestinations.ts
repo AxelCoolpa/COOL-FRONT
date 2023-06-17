de5a59b9d0d99f70dc1137258ba1bdb0d9d8cd57
MrTban <esteman214@gmail.com>
2023-06-17 15:25:05 -0500

    get destinations

---
+++ src/api/getDestinations.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const getDestinationsAPI = async () => {
+	try {
+		const response = await axios('http://localhost:3000/api/destinations')
+		// Aquí puedes realizar el manejo de la respuesta de la API
+		// por ejemplo, validar la respuesta o extraer datos relevantes
+		// y retornar cualquier resultado necesario
+		return response.data // Puedes ajustar esto según la estructura de la respuesta de la API
+	} catch (error: any) {
+		throw new Error(error.message)
+	}
+}

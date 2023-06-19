ae959d713779c2532d5f05474ab66bc898ea5635
Martin Iribas <martin.iribass@gmail.com>
2023-06-19 15:08:15 -0300

    create AppDispatch

--- src/pages/ProvUser/ShowDiscover.tsx
+++ src/pages/ProvUser/ShowDiscover.tsx
@@ -1,16 +1,27 @@
 //   Marto !
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchDestinations } from "../../state/actions/destinationActions";
+import { selectDestinations } from "../../state/selectors/destinationSelectors";
 
 import Button from "../../components/buttons/Button";
 import GridColumns from "../../components/sections/GridColumns";
-import BookingCard from "../../components/details/BookingCard";
 import ListingCard from "../../components/listings/ListingCard";
 import { listings } from "../../mocks/listingsCards";
+import { EnumData } from "../../types";
 
-const ShowDiscover = () => {
+
+
+
+const ShowDiscover: React.FC = () => {
+  const dispatch = useDispatch();
+  const destinations = useSelector(selectDestinations)
   const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(fetchDestinations());
+  }, [dispatch]);
 
   return (
     <>
@@ -20,9 +31,9 @@ const ShowDiscover = () => {
             onClick={() => navigate("/proveedor-admin/create")}
           />
           <GridColumns>
-            {listings.map((data) => (
-              <ListingCard key={data.id} data={data} />
-            ))}
+          {destinations.map((destination: EnumData) => {
+						<ListingCard key={destination.id} data={destination} />
+					})}
           </GridColumns>
       </div>
     </>

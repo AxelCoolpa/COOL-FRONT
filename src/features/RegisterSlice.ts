66af87d90d9acc8ee7ba930e7df25ed4e360c874
SadiLuis <sadiluisrueda.94com>
2023-06-20 11:37:22 -0300

    Try to fix bugs in production with Register and Login / Slice's

--- src/features/RegisterSlice.ts
+++ src/features/RegisterSlice.ts
@@ -1,58 +1,59 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { AppThunk } from "../store/Store";
-import { registerUserAPI } from "../api/registerAPI";
-
-export interface RegisterFormData {
-  username: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-}
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { AppThunk } from '../store/Store'
+import { RegisterFormData } from '../components/Register'
 
 interface RegisterState {
-  isLoading: boolean;
-  error: string | null;
+  isLoading: boolean
+  error: string | null
 }
 
 const initialState: RegisterState = {
   isLoading: false,
   error: null,
-};
+}
 
 const registerSlice = createSlice({
-  name: "register",
+  name: 'register',
   initialState,
   reducers: {
     registerStart: (state) => {
-      state.isLoading = true;
-      state.error = null;
+      state.isLoading = true
+      state.error = null
     },
     registerSuccess: (state) => {
-      state.isLoading = false;
+      state.isLoading = false
+      state.error = null
     },
     registerFailure: (state, action: PayloadAction<string>) => {
-      state.isLoading = false;
-      state.error = action.payload;
+      state.isLoading = false
+      state.error = action.payload
     },
   },
-});
+})
 
-export const { registerStart, registerSuccess, registerFailure } =
-  registerSlice.actions;
+export const { registerStart, registerSuccess, registerFailure } = registerSlice.actions
 
-export default registerSlice.reducer;
+export default registerSlice.reducer
 
-export const registerUser = (formData: RegisterFormData): AppThunk => async (
-  dispatch
-) => {
+export const registerUser = (formData: RegisterFormData): AppThunk => async (dispatch) => {
   try {
-    dispatch(registerStart());
+    dispatch(registerStart())
 
-    // Simulate an asynchronous API call
-    await registerUserAPI(formData);
+    const response = await fetch('https://cool-backend-production.up.railway.app/api/signUp', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
 
-    dispatch(registerSuccess());
-  } catch (error:any) {
-    dispatch(registerFailure(error.message));
+    if (response.ok) {
+      dispatch(registerSuccess())
+    } else {
+      const errorData = await response.json()
+      dispatch(registerFailure(errorData.message))
+    }
+  } catch (error) {
+    dispatch(registerFailure('An error occurred during registration.'))
   }
-};
+}

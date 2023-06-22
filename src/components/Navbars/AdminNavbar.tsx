d57ca6477cfaba796c4c412cbfeb89db2925fe35
MrTban <esteman214@gmail.com>
2023-06-22 17:27:04 -0500

    implementando el nuevo dropdown

--- src/components/Navbars/AdminNavbar.tsx
+++ src/components/Navbars/AdminNavbar.tsx
@@ -6,12 +6,13 @@ import { selectUsers } from '../../features/usersSlice'
 
 import { FiBell, FiHeart, FiSearch } from 'react-icons/fi'
 import { TbMessage } from 'react-icons/tb'
-import { BsDot } from 'react-icons/bs'
+import { BsArrowBarUp, BsDot } from 'react-icons/bs'
 
 import AvatarImg from '../../assets/Avatar.jpg'
 
 import Cool from '../../assets/cool.png'
-import UserDropdown from '../dropdown/UserDropdown'
+import Dropdown from '../dropdown/index'
+import Avatar from '../Avatar'
 
 const Navbar: React.FC = () => {
 	const user = useSelector(selectUsers)
@@ -63,48 +64,208 @@ const Navbar: React.FC = () => {
 						</form>
 
 						<div className='flex gap-9'>
-							<div className='relative cursor-pointer' onClick={() => alert('Mensajes')}>
-								{notifications && (
-									<div>
-										<BsDot
-											size={55}
-											className='absolute text-[#FFC368] -top-6 -right-[26px] '
-										/>
+							{/* start Notification */}
+
+							<Dropdown
+								button={
+									<p className='cursor-pointer'>
+										{notifications && (
+											<div>
+												<BsDot
+													size={55}
+													className='absolute text-[#FFC368] -top-6 -right-[26px]'
+												/>
+											</div>
+										)}
+										<TbMessage size={24} className='text-OrangeCooL' />
+									</p>
+								}
+								animation='origin-[65%_0%] md:origin-top-right transition-all duration-300 ease-in-out'
+								children={
+									<div className='flex w-[360px] flex-col gap-3 rounded-[20px] bg-white p-4 shadow-CooL sm:w-[460px]'>
+										<div className='flex items-center justify-between'>
+											<p className='text-base font-bold'>Notification</p>
+											<p className='text-sm font-bold'>Mark all read</p>
+										</div>
+
+										<button className='flex w-full items-center'>
+											<div className='flex h-full w-[85px] items-center justify-center rounded-xl bg-OrangeToGreen py-4 text-2xl'>
+												<BsArrowBarUp className='text-white' />
+											</div>
+											<div className='ml-2 flex h-full w-full flex-col justify-center rounded-lg px-1 text-sm'>
+												<p className='mb-1 text-left text-base font-bold'>
+													New Update: CooL is very Cool
+												</p>
+												<p className='font-base text-left text-xs'>
+													New Update: CooL is very Cool
+												</p>
+											</div>
+										</button>
+
+										<button className='flex w-full items-center'>
+											<div className='flex h-full w-[85px] items-center justify-center rounded-xl bg-OrangeToGreen py-4 text-2xl'>
+												<BsArrowBarUp className='text-white' />
+											</div>
+											<div className='ml-2 flex h-full w-full flex-col justify-center rounded-lg px-1 text-sm'>
+												<p className='mb-1 text-left text-base font-bold'>
+													New Update: CooL is very Cool
+												</p>
+												<p className='font-base text-left text-xs'>
+													New Update: CooL is very Cool
+												</p>
+											</div>
+										</button>
 									</div>
-								)}
-								<TbMessage size={24} className='text-OrangeCooL' />
-							</div>
-							<div
-								className='relative cursor-pointer'
-								onClick={() => alert('Notificaciones')}
-							>
-								{notifications && (
-									<div>
-										<BsDot
-											size={55}
-											className='absolute text-[#FFC368] -top-6 -right-[26px]'
+								}
+								classNames={'py-2 top-4 -left-[230px] md:-left-[440px] w-max'}
+							/>
+
+							<Dropdown
+								button={
+									<p className='cursor-pointer'>
+										{notifications && (
+											<div>
+												<BsDot
+													size={55}
+													className='absolute text-[#FFC368] -top-6 -right-[26px]'
+												/>
+											</div>
+										)}
+										<FiBell size={24} className='text-OrangeCooL' />
+									</p>
+								}
+								children={
+									<div className='flex w-[350px] flex-col gap-2 rounded-[20px] bg-white p-4 shadow-CooL'>
+										<div
+											style={{
+												backgroundImage: `url(${AvatarImg})`,
+												backgroundRepeat: 'no-repeat',
+												backgroundSize: 'cover',
+											}}
+											className='mb-2 aspect-video w-full rounded-lg'
 										/>
+										<a
+											href=' '
+											className='px-full linear flex cursor-pointer items-center justify-center rounded-xl bg-OrangeCooL py-[11px] font-bold transition duration-200'
+										>
+											CooL is very Cool
+										</a>
+										<a
+											href=' '
+											className='px-full linear flex cursor-pointer items-center justify-center rounded-xl border py-[11px] font-bold transition duration-200'
+										>
+											CooL is very Cool
+										</a>
+										<a
+											href=' '
+											className='hover:bg-black px-full linear flex cursor-pointer items-center justify-center rounded-xl py-[11px] font-bold transition duration-200'
+										>
+											CooL is very Cool
+										</a>
 									</div>
-								)}
-								<FiBell size={24} className='text-OrangeCooL' />
-							</div>
-							<div className='relative cursor-pointer' onClick={() => alert('Favoritos')}>
-								{notifications && (
-									<div>
-										<BsDot
-											size={55}
-											className='absolute text-[#FFC368] -top-6 -right-[26px] '
+								}
+								classNames={'py-2 top-6 -left-[250px] md:-left-[330px] w-max'}
+								animation='origin-[75%_0%] md:origin-top-right transition-all duration-300 ease-in-out'
+							/>
+
+							<Dropdown
+								button={
+									<p className='cursor-pointer'>
+										{notifications && (
+											<div>
+												<BsDot
+													size={55}
+													className='absolute text-[#FFC368] -top-6 -right-[26px] '
+												/>
+											</div>
+										)}
+										<FiHeart size={24} className='text-OrangeCooL' />
+									</p>
+								}
+								children={
+									<div className='flex w-[350px] flex-col gap-2 rounded-[20px] bg-white p-4 shadow-CooL'>
+										<div
+											style={{
+												backgroundImage: `url(${AvatarImg})`,
+												backgroundRepeat: 'no-repeat',
+												backgroundSize: 'cover',
+											}}
+											className='mb-2 aspect-video w-full rounded-lg'
 										/>
+										<a
+											href=' '
+											className='px-full linear flex cursor-pointer items-center justify-center rounded-xl bg-OrangeCooL py-[11px] font-bold transition duration-200 text-white'
+										>
+											CooL is very Cool
+										</a>
+										<a
+											href=' '
+											className='px-full linear flex cursor-pointer items-center justify-center rounded-xl border py-[11px] font-bold transition duration-200'
+										>
+											CooL is very Cool
+										</a>
+										<a
+											href=' '
+											className='hover:bg-black px-full linear flex cursor-pointer items-center justify-center rounded-xl py-[11px] font-bold transition duration-200'
+										>
+											CooL is very Cool
+										</a>
 									</div>
-								)}
-								<FiHeart size={24} className='text-OrangeCooL' />
-							</div>
+								}
+								classNames={'py-2 top-6 -left-[250px] md:-left-[330px] w-max'}
+								animation='origin-[75%_0%] md:origin-top-right transition-all duration-300 ease-in-out'
+							/>
 						</div>
 						<hr className='w-12 rotate-90' />
 						{/* User */}
 						<ul className='flex-col md:flex-row list-none items-center hidden md:flex'>
 							<div className='flex items-center gap-6'>
-								<UserDropdown avatar={AvatarImg} />
+								<Dropdown
+									button={<Avatar avatar={AvatarImg} wh={12} />}
+									children={
+										<div className='flex h-48 w-56 flex-col justify-start rounded-[20px] bg-white bg-cover bg-no-repeat shadow-xl'>
+											<div className='mt-3 ml-4'>
+												<div className='flex items-center gap-2'>
+													<p className='text-sm font-bold cursor-default'>
+														👋 Hey, {userProvider?.username}
+													</p>
+												</div>
+											</div>
+											<div className='mt-3 mx-4 flex flex-col'>
+												<div className='h-px w-full bg-gray-200' />
+												<a
+													href='#'
+													className={
+														'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
+													}
+													onClick={(e) => e.preventDefault()}
+												>
+													Another action
+												</a>
+												<a
+													href='#'
+													className={
+														'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
+													}
+													onClick={(e) => e.preventDefault()}
+												>
+													Something else here
+												</a>
+												<div className='mt-3 h-px w-full bg-gray-200' />
+												<a
+													href='#'
+													className={
+														'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
+													}
+													onClick={(e) => e.preventDefault()}
+												>
+													Seprated link
+												</a>
+											</div>
+										</div>
+									}
+									classNames={'py-2 top-8 -left-[180px] w-max'}
+								/>
 								<div className='flex flex-col justify-center'>
 									<label className='2xl:text-lg font-semibold'>
 										{userProvider?.username}

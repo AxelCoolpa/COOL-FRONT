7a5fba08bfbae4e787b07562d75464a3df878b36
MrTban <esteman214@gmail.com>
2023-06-22 19:11:12 -0500

    button navigate to provider/create

--- src/pages/ProvUser/ShowDiscover.tsx
+++ src/pages/ProvUser/ShowDiscover.tsx
@@ -1,71 +1,71 @@
 //   Marto !
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import {
-  selectDestinations,
-  selectError,
-  selectLoading,
-} from "../../features/destinationSlice";
+	selectDestinations,
+	selectError,
+	selectLoading,
+} from '../../features/destinationSlice'
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Thumbs } from "swiper";
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Thumbs } from 'swiper'
 
-import GridColumns from "../../components/sections/GridColumns";
-import ListingCard from "../../components/listings/ListingCard";
+import GridColumns from '../../components/sections/GridColumns'
+import ListingCard from '../../components/listings/ListingCard'
 
 const ShowDiscover: React.FC = () => {
-  const destinations = useSelector(selectDestinations);
-  const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
-  const navigate = useNavigate();
+	const destinations = useSelector(selectDestinations)
+	const loading = useSelector(selectLoading)
+	const error = useSelector(selectError)
+	const navigate = useNavigate()
 
-  if (loading) {
-    return <div>Cargando destinos...</div>;
-  }
-  if (error) {
-    return <div>Error al cargar destinos: {error} </div>;
-  }
+	if (loading) {
+		return <div>Cargando destinos...</div>
+	}
+	if (error) {
+		return <div>Error al cargar destinos: {error} </div>
+	}
 
-  const validDestinations = destinations.filter(
-    (destination) => destination !== undefined && destination !== null
-  );
+	const validDestinations = destinations.filter(
+		(destination) => destination !== undefined && destination !== null
+	)
 
-  return (
-    <>
-      <div className="flex flex-wrap">
-        <div className="rounded-t bg-white mb-0 px-6 py-6">
-          <div className="text-center flex justify-between pt-3 mt-3 mb-3 pb-3">
-            <h6 className="text-gray-600 text-md uppercase font-bold">
-              Active adventures
-            </h6>
-            <button
-              className="bg-red-500 text-white active:bg-red-200 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md mr-1 ease-linear transition-all duration-150"
-              onClick={() => navigate("/proveedor-admin/create")}
-            >
-              Add Adventure
-            </button>
-          </div>
+	return (
+		<>
+			<div className='flex flex-wrap'>
+				<div className='rounded-t bg-white mb-0 px-6 py-6'>
+					<div className='text-center flex justify-between pt-3 mt-3 mb-3 pb-3'>
+						<h6 className='text-gray-600 text-md uppercase font-bold'>
+							Active adventures
+						</h6>
+						<button
+							className='bg-red-500 text-white active:bg-red-200 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md mr-1 ease-linear transition-all duration-150'
+							onClick={() => navigate('/provider/create')}
+						>
+							Add Adventure
+						</button>
+					</div>
 
-          <div className="flex-auto px-3 lg:px-0 py-10 pt-0">
-            <GridColumns>
-              {validDestinations.map((destination) => (
-                <ListingCard key={destination._id} data={destination} />
-              ))}
-            </GridColumns>
-          </div>
-        </div>
+					<div className='flex-auto px-3 lg:px-0 py-10 pt-0'>
+						<GridColumns>
+							{validDestinations.map((destination) => (
+								<ListingCard key={destination._id} data={destination} />
+							))}
+						</GridColumns>
+					</div>
+				</div>
 
-        <div className="rounded-t bg-white mb-0 px-6 py-6">
-          <div className="text-center flex justify-between pt-3 mt-3 mb-3 pb-3">
-            <h6 className="text-gray-600 text-md uppercase font-bold">
-              Inactive adventures
-            </h6>
-          </div>
+				<div className='rounded-t bg-white mb-0 px-6 py-6'>
+					<div className='text-center flex justify-between pt-3 mt-3 mb-3 pb-3'>
+						<h6 className='text-gray-600 text-md uppercase font-bold'>
+							Inactive adventures
+						</h6>
+					</div>
 
-            <div className="flex-auto px-3 lg:px-0 py-10 pt-0">
-            <GridColumns>
-            {/* <Swiper
+					<div className='flex-auto px-3 lg:px-0 py-10 pt-0'>
+						<GridColumns>
+							{/* <Swiper
               loop={true}
               spaceBetween={10}
               navigation={true}
@@ -74,17 +74,17 @@ const ShowDiscover: React.FC = () => {
               className="product-images-slider"
             >
               <SwiperSlide> */}
-              {validDestinations.map((destination) => (
-                <ListingCard key={destination._id} data={destination} />
-                ))}
-                {/* </SwiperSlide>
+							{validDestinations.map((destination) => (
+								<ListingCard key={destination._id} data={destination} />
+							))}
+							{/* </SwiperSlide>
             </Swiper> */}
-            </GridColumns>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+						</GridColumns>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
 
-export default ShowDiscover;
+export default ShowDiscover

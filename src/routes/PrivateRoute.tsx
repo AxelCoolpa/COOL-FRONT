258740a7098f8caa1b70ca2b0eb225c87f7c394f
Martin Iribas <martin.iribass@gmail.com>
2023-06-23 11:14:28 -0300

    private route refact. roles & isauthenticated

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,15 +1,21 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RouteProps } from "react-router-dom";
-import { RootState } from '../store'
-import LoginPage from "../pages/LoginPage";
-import { checkAuthentication } from "../features/authSlice";
+import React, { ReactNode } from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { RootState } from "../store/Store";
 
-interface Props {
-  privateValidation: boolean;
+interface ProtectedRouteProps {
+	children: ReactNode;
+	requiredRole: string;
 }
 
-const authValidationUser = ({ validationUser }: Props) => {
-	const userState = useSelector((store: RootState) => store.auth)
-	return userState.isAuthenticated ?  privateValidationUser : publicValidationUser && <LoginPage />
-}
+const ProtectedRoute: React.FC<ProtectedRouteProps>  = ({ children, requiredRole }) => {
+	const { isAuthenticated, userRole } = useSelector((state: RootState) => state.auth);
+  
+	if (isAuthenticated && userRole === requiredRole) {
+	  return <>{children}</>;
+	}
+	
+	return <Navigate to="/login" />;
+  };
+  
+  export default ProtectedRoute;

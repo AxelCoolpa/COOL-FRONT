16f703d569d87d92644e41decb3bc0c7a4a74fdd
MrTban <esteman214@gmail.com>
2023-06-23 10:34:46 -0500

    fix categories error

--- src/components/sections/AddAdventure.tsx
+++ src/components/sections/AddAdventure.tsx
@@ -50,6 +50,8 @@ const AddAdventure = () => {
 		endTime: [],
 	})
 
+	formData.categories = checkboxValues
+
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = e.target
 		setFormData((prevFormData) => ({
@@ -65,6 +67,7 @@ const AddAdventure = () => {
 			gallery: updatedGallery,
 		}))
 	}
+
 	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault()
 
@@ -107,7 +110,7 @@ const AddAdventure = () => {
 			data.append('gallery', formData.gallery[i])
 		}
 
-		formData.categories.map((category) => {
+		formData.categories.forEach((category) => {
 			data.append('categories', category)
 		})
 

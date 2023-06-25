fde7b0f104afa267af95775085036a30015e4dd2
Martin Iribas <martin.iribass@gmail.com>
2023-06-24 21:33:16 -0300

    perfil provider

--- src/components/cards/ProfileProviderCard.tsx
+++ src/components/cards/ProfileProviderCard.tsx
@@ -1,3 +1,4 @@
+import Avatar from "../Avatar";
 
 
 
@@ -10,11 +11,7 @@ const ProfileProviderCard = () => {
             <div className="flex flex-wrap justify-center">
               <div className="w-full px-4 flex justify-center">
                 <div className="relative">
-                  {/* <img
-                    alt="..."
-                    src={require("assets/img/team-2-800x800.jpg").default}
-                    className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
-                  /> */}
+                  <Avatar />
                 </div>
               </div>
               <div className="w-full px-4 text-center mt-20">
@@ -41,7 +38,7 @@ const ProfileProviderCard = () => {
               </div>
             </div>
             <div className="text-center mt-12">
-              <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
+              <h3 className="text-xl font-semibold leading-normal text-blueGray-700 mb-2">
                 Jenna Stones
               </h3>
               <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">

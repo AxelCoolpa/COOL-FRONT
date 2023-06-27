e2c57e05a41bddb3c7b3a2f4b664bc2f61d07c99
MrTban <esteman214@gmail.com>
2023-06-27 18:39:56 -0500

    delete route

--- src/api/deleteDestination.ts
+++ src/api/deleteDestination.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { baseURL } from '../baseURL'
 
-export const deleteDestinationAPI = async (destinationID: string) => {
+export const deleteDestinationAPI = async (destinationID: string): Promise<void> => {
 	try {
 		const response = await axios.delete(`${baseURL}/destination/${destinationID}`)
 		return response.data

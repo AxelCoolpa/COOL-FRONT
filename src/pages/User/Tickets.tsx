a4a0e81b3e03bf8ee6663804581f67e7d42e2f21
MrTban <esteman214@gmail.com>
2023-06-27 14:16:49 -0500

    mapping tickets mock

--- src/pages/User/Tickets.tsx
+++ src/pages/User/Tickets.tsx
@@ -1,14 +1,17 @@
 import Ticket from '../../components/listings/Ticket'
 
+import { listingTickets } from '../../mocks/listingTickets'
+
 const Tickets = () => {
 	return (
 		<div>
 			<h1 className='text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold pt-5 pb-20  text-center tracking-tighter'>
 				Tickets
 			</h1>
-			<div className='pt-10 grid grid-cols-1 min-[1360px]:grid-cols-2 gap-14'>
-				<Ticket />
-				<Ticket />
+			<div className='py-10 flex flex-wrap items-center justify-center gap-14'>
+				{listingTickets.map((ticket) => (
+					<Ticket key={ticket._id} data={ticket} />
+				))}
 			</div>
 		</div>
 	)

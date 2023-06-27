7da8a1ebbb1039251ac863ea6a75f886ac0bb6a3
MrTban <esteman214@gmail.com>
2023-06-27 13:53:51 -0500

    tickets mock

---
+++ src/mocks/listingTickets.ts
@@ -0,0 +1,34 @@
+export const listingTickets = [
+	{
+		_id: '399202849211238',
+		destination: 'Canal de panamá',
+		activity: 'Visit basic',
+		date: '20 Jun',
+		time: '19:00 hs',
+		peoples: '2 adults',
+	},
+	{
+		_id: '492112383992028',
+		destination: 'Cancún',
+		activity: 'Divin',
+		date: '15 Aug',
+		time: '10:00 hs',
+		peoples: '4 adults',
+	},
+	{
+		_id: '920284921123839',
+		destination: 'Roma',
+		activity: 'Tour',
+		date: '25 Jul',
+		time: '03:00 hs',
+		peoples: '3 Customers',
+	},
+	{
+		_id: '112383992028492',
+		destination: 'Japan',
+		activity: 'International visit',
+		date: '28 Jun',
+		time: '18:00 hs',
+		peoples: '4 Customers',
+	},
+]

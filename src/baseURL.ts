393eedbbd1a051df719b14e6ca6d8c03bb1f12a0
Martin Iribas <martin.iribass@gmail.com>
2023-06-30 20:08:06 -0300

    feat(baseurl): nuevo deploy

--- src/baseURL.ts
+++ src/baseURL.ts
@@ -1,3 +1,4 @@
-export const baseURL = 'https://cool-backend-production.up.railway.app/api'
+export const baseURL = 'http://cool-travels.alwaysdata.net/api'
+// export const baseURL = 'https://cool-backend-production.up.railway.app/api'
 //export const baseURL = 'http://localhost:3000/api'
 

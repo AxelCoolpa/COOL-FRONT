2016f2880864ebdfc7c56d346e1d1583822031e4
MrTban <esteman214@gmail.com>
2023-07-04 14:23:08 -0500

    cleaning the file

--- src/components/cloudinary/ImageUpload.tsx
+++ src/components/cloudinary/ImageUpload.tsx
@@ -3,19 +3,8 @@ import axios from 'axios'
 import { TbPhotoPlus } from 'react-icons/tb'
 import { IoMdClose } from 'react-icons/io'
 
-{
-	/* <CloudinaryUploadImg
-	onUpload={handleUpload}
-	name='picture'
-	onClick={handleChange}
-	className='pb-10'
-/> */
-}
-
 interface CloudinaryUploadProps {
 	onUpload: (e: React.ChangeEvent<HTMLInputElement>) => void
-	name: string
-	onClick: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 const CloudinaryUploadImg: React.FC<CloudinaryUploadProps> = ({ onUpload }) => {
@@ -68,7 +57,7 @@ const CloudinaryUploadImg: React.FC<CloudinaryUploadProps> = ({ onUpload }) => {
 						</div>
 					)}
 					<p className='my-2 text-sm text-gray-500 dark:text-gray-400'>
-						<span className='font-semibold'>Click to select</span> or drag and drop
+						<span className='font-semibold'>Click to select image</span>
 					</p>
 					<p className='text-xs text-gray-500 dark:text-gray-400'>SVG, PNG, JPG or GIF</p>
 				</div>
@@ -94,46 +83,3 @@ const CloudinaryUploadImg: React.FC<CloudinaryUploadProps> = ({ onUpload }) => {
 }
 
 export default CloudinaryUploadImg
-
-/* <div className='flex'>
-				<div className='flex'>
-					<div className='shrink-0'>
-						<label
-							htmlFor='about'
-							className='mt-2 ml-4 block text-sm font-medium leading-6 text-gray-900'
-						>
-							Foto seleccionada
-							<img
-								className='h-32 w-32 object-cover rounded-full'
-								src={
-									image
-										? image
-										: `https://res.cloudinary.com/dpxucxgwg/image/upload/v1679450694/anonimo_uim8xm.png`
-								}
-								alt='imagen cargada'
-							/>
-						</label>
-					</div>
-					<label className='block pl-6 py-11'>
-						<span className='sr-only'>Selecciona una foto de perfil</span>
-						<input
-							type='file'
-							className='block w-full text-lg text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-lg file:font-semibold file:bg-violet-50 file:lighter-blue hover:file:bg-violet-100'
-							accept='.png, .jpg, .jpeg .webp .gif. .svg'
-							name=''
-							id=''
-							onChange={(e) => {
-								setImageSelected(e.target.files[0])
-							}}
-						/>
-					</label>
-
-					<button
-						type='button'
-						className='ml-6 my-11 lighter-blue mr-4 py-2 px-4 rounded-full border-0 text-lg font-semibold bg-violet-100 h-fit'
-						onClick={uploadImage}
-					>
-						Cargar Imagen
-					</button>
-				</div>
-			</div> */

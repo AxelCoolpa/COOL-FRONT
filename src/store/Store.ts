2dd9b9a9e69450d727b36e9b10e4981cf840691c
Martin Iribas <martin.iribass@gmail.com>
2023-07-04 12:42:05 -0300

    feat(store): apply middleware query

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -16,6 +16,8 @@ import deleteActivitySlice from '../features/deleteActivitySlice'
 import activitiesSlice from '../features/activitiesSlice'
 import activityByIdSlice from '../features/activityByIdSlice'
 import updateUserSlice from '../features/updateUserSlice'
+import { userApi } from '../api/getUsers'
+import { setupListeners } from '@reduxjs/toolkit/dist/query'
 
 export const store = configureStore({
 	reducer: {
@@ -27,6 +29,7 @@ export const store = configureStore({
 		user: usersSlice,
 		userById: userByIdSlice,
 		updateUser: updateUserSlice,
+		[userApi.reducerPath]: userApi.reducer,
 		//* Destinations
 		destination: destinationSlice,
 		destinationById: destinationsByIdSlice,
@@ -39,8 +42,14 @@ export const store = configureStore({
 		createActivity: createActiviySlice,
 		updateActivity: updateActivitySlice,
 		deleteActivity: deleteActivitySlice,
-	},
+	}, 
+	// middleware para solicitar datos asincronos del backend
+	middleware: (getDefaultMiddleware) => 
+		getDefaultMiddleware().concat([userApi.middleware])
 })
+
+//para ejecutar actions dentro del store
+setupListeners(store.dispatch) 
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch

399bd80c1a4bd9f835a5ab6fb95c4c1e7d53d0b4
Martin Iribas <martin.iribass@gmail.com>
2023-07-04 14:19:44 -0300

    feat(layouts): add navUser & sidebarUser

--- src/layouts/User.tsx
+++ src/layouts/User.tsx
@@ -1,25 +1,26 @@
-import { Outlet } from 'react-router-dom'
-
-import SideBar from '../components/sidebar/SideBar'
-import DashContainer from '../components/sections/dashContainer'
 import { useEffect } from 'react'
-import { fetchDestinations } from '../features/destinationSlice'
 import { useDispatch } from 'react-redux'
+import { Outlet } from 'react-router-dom'
 import { fetchUsers } from '../features/usersSlice'
+import { fetchDestinations, fetchDestinationsStart } from '../features/destinationSlice'
+import UserNav from '../components/Navbars/UserNav'
+import SidebarUser from '../components/sidebar/SidebarUser'
+import DashContainer2 from '../components/sections/dashContainer2'
 
 export const User = () => {
 	const dispatch = useDispatch()
-
 	useEffect(() => {
 		dispatch(fetchDestinations())
 		dispatch(fetchUsers())
 	}, [])
 	return (
 		<>
-			<SideBar />
-			<DashContainer>
+			<SidebarUser />
+			<DashContainer2>
+				<UserNav />
+			<div className='md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-20 flex flex-wrap items-center justify-between w-full mx-auto'>
 				<Outlet />
-			</DashContainer>
+			</div>
+			</DashContainer2>
 		</>
-	)
-}
+)}

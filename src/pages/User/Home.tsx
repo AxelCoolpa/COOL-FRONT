5c67e43ea035293b1c4b5092763745f1e487ba09
MrTban <esteman214@gmail.com>
2023-07-07 13:46:10 -0500

    ...

--- src/pages/User/Home.tsx
+++ src/pages/User/Home.tsx
@@ -5,25 +5,25 @@ import DestinationCard from '../../components/listings/DestinationCard'
 import ListingCardnew from '../../components/listings/ListingCard'
 import { useDispatch, useSelector } from 'react-redux'
 import {
-  fetchDestinationsStart,
-  selectDestinations,
-  selectError,
-  selectLoading,
-} from "../../features/destinationSlice";
-import { useEffect, useState } from "react";
-import HeaderSection from "../../components/sections/HeaderSection";
-import Loading from "../../components/loadings/Loading";
-import { useDestinations } from '../../hooks/useDestination';
+	fetchDestinationsStart,
+	selectDestinations,
+	selectError,
+	selectLoading,
+} from '../../features/destinationSlice'
+import { useEffect, useState } from 'react'
+import HeaderSection from '../../components/sections/HeaderSection'
+import Loading from '../../components/loadings/Loading'
+import { useDestinations } from '../../hooks/useDestination'
 
 const Home = () => {
 	const { destinos } = useDestinations()
-	const destino = destinos?.map((dest) => dest);
-	const destinations = useSelector(selectDestinations);
-	const [checkboxValues, setCheckboxValues] = useState([]);
+	const destino = destinos?.map((dest) => dest)
+	const destinations = useSelector(selectDestinations)
+	const [checkboxValues, setCheckboxValues] = useState([])
 	const loading = useSelector(selectLoading)
 	const error = useSelector(selectError)
 	/* const dispatch = useDispatch(); */
-	
+
 	if (loading) {
 		return <div>Cargando destinos...</div>
 	}
@@ -38,55 +38,54 @@ const validDestinations = destinations.filter(
 	(destination) => destination !== undefined && destination !== null
 )
  */
-
-const filteredDestinations = destino?.filter((destination) => {
-	if (checkboxValues.length === 0) {
-		console.log("ninguna seleccionada");
-		return true;
-	} else {
-		return checkboxValues.some((category) =>
-		  destination.categories.includes(category)
-		  );
+	const filteredDestinations = destino?.filter((destination) => {
+		if (checkboxValues.length === 0) {
+			console.log('ninguna seleccionada')
+			return true
+		} else {
+			return checkboxValues.some((category) => destination.categories.includes(category))
 		}
 	})
 
-	const discover = filteredDestinations[Math.floor(Math.random() * destinations.length)]
+	/* const discover = destinos?[Math.floor(Math.random() * destinations.length)] */
 
 	return (
 		<>
 			<div className='mt-12 gap-3 lg:mx-3'>
-				<HeaderSection
-					title='THE PLACE OF YOUR DREAMS'
-					subtitle='Explore the best destinations in the world'
-					image={discover?.galleryImage}
-				/>
+				{/* <HeaderSection
+				title='THE PLACE OF YOUR DREAMS'
+				subtitle='Explore the best destinations in the world'
+				image={destino.[0]} />
 
-      <Card className="mt-12 gap-3 lg:mx-3">
-        <Card>
-          <CardBody>
-            <div className="flex px-5">
-              {!loading && !error && (
-                <div className=" grid grid-cols-1 min-[950px]:grid-cols-2 min-[1200px]:grid-cols-3 min-[1440px]:grid-cols-4 min-[1540px]:grid-cols-5 min-[1640px]:grid-cols-6 gap-1">
-                  {destino?.map((destination) => (
-                    <DestinationCard key={destination._id} data={destination} />
-                  ))}
-                </div>
-            )}
-            </div>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardBody>
-            {/* <ul className="flex px-9 flex-col gap-6">
+
+				
+ */}
+				<Card className='mt-12 gap-3 lg:mx-3'>
+					<Card>
+						<CardBody>
+							<div className='flex px-5'>
+								{!loading && !error && (
+									<div className=' grid grid-cols-1 min-[950px]:grid-cols-2 min-[1200px]:grid-cols-3 min-[1440px]:grid-cols-4 min-[1540px]:grid-cols-5 min-[1640px]:grid-cols-6 gap-1'>
+										{destino?.map((destination) => (
+											<DestinationCard key={destination._id} data={destination} />
+										))}
+									</div>
+								)}
+							</div>
+						</CardBody>
+					</Card>
+					<Card>
+						<CardBody>
+							{/* <ul className="flex px-9 flex-col gap-6">
               <ListingCardnew data={destinos} />
             </ul> */}
-          </CardBody>
-        </Card>
-        <div className="gird-cols-1 mb-12 grid gap-12 px-4 lg:grid-cols-2 xl:grid-cols-3"></div>
-        <div className="px-4 pb-4"></div>
-      </Card>
-	  </div>
-    </>
+						</CardBody>
+					</Card>
+					<div className='gird-cols-1 mb-12 grid gap-12 px-4 lg:grid-cols-2 xl:grid-cols-3'></div>
+					<div className='px-4 pb-4'></div>
+				</Card>
+			</div>
+		</>
 	)
 }
 

cafec8ec73236541731ea9c6cb9c0bbbe6b19007
Martin Iribas <martin.iribass@gmail.com>
2023-07-07 21:26:57 -0300

    feat(headerDetail): adaptando nuevas llamadas galleryImage

--- src/components/details/DetailHeader.tsx
+++ src/components/details/DetailHeader.tsx
@@ -10,18 +10,18 @@ const DetailHeader: React.FC<DetailHeaderProps> = ({ listing }) => {
 		<div className='w-full max-h-[300px] lg:max-h-[400px]'>
 			<div className='flex gap-5 lg:justify-center'>
 				<img
-					src={listing?.galleryImage || listing?.gallery[0]}
+					src={listing?.galleryImage || listing?.galleryImage[0]}
 					className='object-cover w-3/5 xl:w-4/5 h-[300px] lg:h-[400px] rounded-[20px]'
 				/>
-				{listing?.gallery && (
+				{listing?.galleryImage && (
 					<div className='flex flex-col object-cover gap-5'>
 						<img
-							src={listing?.gallery[1]}
+							src={listing?.galleryImage[1]}
 							alt=''
 							className='object-cover h-[140px] lg:h-[190px] rounded-[20px]'
 						/>
 						<img
-							src={listing?.gallery[2]}
+							src={listing?.galleryImage[2]}
 							alt=''
 							className='object-cover h-[140px] lg:h-[190px] rounded-[20px]'
 						/>

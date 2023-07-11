1bce9608f4e810d59b8139dab475538fcbc3d66b
MrTban <esteman214@gmail.com>
2023-07-11 08:40:13 -0500

    conditional render by itDeleted

--- src/pages/ProvUser/ShowActivities.tsx
+++ src/pages/ProvUser/ShowActivities.tsx
@@ -1,23 +1,18 @@
-//   Marto !
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import {
-	selectDestinations,
-	selectError,
-	selectLoading,
-} from '../../features/destinationSlice'
-
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Thumbs } from 'swiper'
+import { selectError, selectLoading } from '../../features/destinationSlice'
 
 import GridColumns from '../../components/sections/GridColumns'
 import ListingCard from '../../components/listings/ListingCard'
 import { selectActivities } from '../../features/activitiesSlice'
+import { useCurrentUser } from '../../hooks/useCurrentUser'
+import ActivityCard from '../../components/listings/ActivityCard'
 
 const ShowActivities: React.FC = () => {
 	const activities = useSelector(selectActivities)
-	console.log(activities)
+
+	const { currentUser } = useCurrentUser()
 
 	const loading = useSelector(selectLoading)
 	const error = useSelector(selectError)
@@ -44,20 +39,42 @@ const ShowActivities: React.FC = () => {
 				<div className='flex flex-col rounded-t bg-white mb-0 px-6 py-6'>
 					<div className='text-center flex justify-between pt-3 mt-3 mb-3 pb-3'>
 						<h6 className='text-gray-600 text-md uppercase font-bold'>
-							Active activities
+							{currentUser?.profileProvider?.relatedChannel === 'Travel'
+								? 'Active activities'
+								: currentUser?.profileProvider?.relatedChannel === 'Accomodation'
+								? 'Active rooms'
+								: currentUser?.profileProvider?.relatedChannel === 'Logistics'
+								? 'Active services'
+								: null}
 						</h6>
-						<button
-							className='bg-red-500 text-white active:bg-red-200 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md mr-1 ease-linear transition-all duration-150'
-							onClick={() => navigate('/provider/create')}
-						>
-							Add Activity
-						</button>
+						{currentUser?.profileProvider?.relatedChannel === 'Travel' ? (
+							<button
+								className='bg-red-500 text-white active:bg-red-200 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md mr-1 ease-linear transition-all duration-150'
+								onClick={() => navigate('/provider/create/activity')}
+							>
+								Add Activity
+							</button>
+						) : currentUser?.profileProvider?.relatedChannel === 'Accomodation' ? (
+							<button
+								className='bg-red-500 text-white active:bg-red-200 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md mr-1 ease-linear transition-all duration-150'
+								onClick={() => navigate('/provider/create/accomodation')}
+							>
+								Add Accomodation
+							</button>
+						) : currentUser?.profileProvider?.relatedChannel === 'Logistics' ? (
+							<button
+								className='bg-red-500 text-white active:bg-red-200 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md mr-1 ease-linear transition-all duration-150'
+								onClick={() => navigate('/provider/create/logistic')}
+							>
+								Add Logistics
+							</button>
+						) : null}
 					</div>
 
 					<div className='flex-auto px-3 lg:px-0 py-10 pt-0'>
 						<GridColumns>
 							{enabledActitivities.map((activity) => (
-								<ListingCard key={activity._id} data={activity} />
+								<ActivityCard key={activity._id} data={activity} />
 							))}
 						</GridColumns>
 					</div>

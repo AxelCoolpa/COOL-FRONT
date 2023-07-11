f465895d342c18bf0b2309cfa1cb68912c3dedba
MrTban <esteman214@gmail.com>
2023-07-11 10:53:38 -0500

    Add Acomodation View created

--- src/components/sections/AddAccomodation.tsx
+++ src/components/sections/AddAccomodation.tsx
@@ -1,5 +1,242 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
+import { useCurrentUser } from '../../hooks/useCurrentUser'
+
+import { toast } from 'react-hot-toast'
+
+import { categories } from '../categories/categories'
+
+import AccomodationForm from '../forms/AccomodationForm'
+import CategoryInput from '../inputs/CategoryInput'
+import Map from '../Map'
+import Button from '../buttons/Button'
+import Container from '../containers/Container'
+import DropZone from '../inputs/DropZone'
+import { amenitiesCategories } from '../categories/amenitiesCategories'
+
 const AddAccomodation = () => {
-	return <div>AddAccomodation</div>
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+
+	const { currentUserId } = useCurrentUser()
+
+	const [formData, setFormData] = useState({
+		title: '',
+		description: '',
+		location: '',
+		galleryImage: [],
+		amenities: [],
+		category: [],
+		individualPrice: '',
+		groupPrice: '',
+		startTime: '',
+		endTime: '',
+	})
+
+	const handleCheckboxZoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value
+		const isChecked = e.target.checked
+
+		if (isChecked) {
+			setFormData((prevFormData) => ({
+				...prevFormData,
+				category: [...formData.category, value],
+			}))
+		} else {
+			setFormData((prevFormData) => ({
+				...prevFormData,
+				category: formData.category.filter((val) => val !== value),
+			}))
+		}
+	}
+
+	const handleCheckboxAmenitiesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value
+		const isChecked = e.target.checked
+
+		if (isChecked) {
+			setFormData((prevFormData) => ({
+				...prevFormData,
+				amenities: [...formData.amenities, value],
+			}))
+		} else {
+			setFormData((prevFormData) => ({
+				...prevFormData,
+				amenities: formData.amenities.filter((val) => val !== value),
+			}))
+		}
+	}
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target
+		setFormData((prevFormData) => ({
+			...prevFormData,
+			[name]: value,
+		}))
+	}
+
+	const handleFilesSelected = (files: File[]) => {
+		const updatedGallery = [...formData.galleryImage, ...files]
+		setFormData((prevFormData) => ({
+			...prevFormData,
+			galleryImage: updatedGallery,
+		}))
+	}
+
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault()
+
+		const {
+			title,
+			description,
+			individualPrice,
+			groupPrice,
+			galleryImage,
+			amenities,
+			category,
+			location,
+			startTime,
+			endTime,
+		} = formData
+
+		if (
+			!title ||
+			!description ||
+			!individualPrice ||
+			!groupPrice ||
+			!galleryImage ||
+			!category ||
+			!location ||
+			!amenities ||
+			!startTime ||
+			!endTime
+		) {
+			toast.error('Por favor, complete todos los campos')
+			return
+		}
+
+		const data = new FormData()
+		data.append('title', title)
+		data.append('description', description)
+		data.append('individualPrice', individualPrice)
+		data.append('groupPrice', groupPrice)
+		data.append('location', location)
+		data.append('startTime', startTime)
+		data.append('endTime', endTime)
+
+		for (let i = 0; i < formData.galleryImage.length; i++) {
+			data.append('galleryImage', formData.galleryImage[i])
+		}
+
+		formData.amenities.forEach((amenity) => {
+			data.append('amenities', amenity)
+		})
+
+		formData.category.forEach((category) => {
+			data.append('category', category)
+		})
+
+		try {
+			dispatch(createAccomodation(formData, currentUserId))
+			setFormData({
+				title: '',
+				description: '',
+				location: '',
+				galleryImage: [],
+				amenities: [],
+				category: [],
+				individualPrice: '',
+				groupPrice: '',
+				startTime: '',
+				endTime: '',
+			})
+		} catch (error: any) {
+			toast.error('Error al enviar el formulario:', error)
+		}
+	}
+
+	return (
+		<Container>
+			<div className='flex flex-col md:items-center xl:items-start pt-14'>
+				<h2 className='text-[32px] font-medium'>Add Accomodation</h2>
+				<form
+					onSubmit={handleSubmit}
+					className='flex flex-col items-center justify-center w-full transition'
+				>
+					{/* IMAGE */}
+					<div className='mx-auto py-5 xl:py-8 w-full xl:w-4/5 2xl:w-5/6'>
+						<DropZone onFilesSelected={handleFilesSelected} />
+					</div>
+
+					{/* FORM */}
+					<div className='w-full xl:w-4/5 2xl:w-5/6 flex items-center justify-center md:gap-10 py-5 xl:py-8'>
+						<AccomodationForm handleChange={handleChange} form={formData} />
+					</div>
+
+					{/* CATEGORIES */}
+					<h3 className='text-2xl font-semibold mx-auto py-5 xl:py-8 xl:w-4/5 2xl:w-5/6'>
+						Amenities
+					</h3>
+
+					<div className='flex flex-wrap col-span-5 gap-10 xl:gap-10 2xl:gap-20 items-center justify-center mx-auto py-5 xl:w-4/5 2xl:w-5/6'>
+						{amenitiesCategories.map((item) => (
+							<ul key={item.label}>
+								<CategoryInput
+									handleChange={handleCheckboxAmenitiesChange}
+									label={item.label}
+									icon={item.icon}
+									id={item.label}
+									name={item.label}
+									value={item.label}
+									bgColor={item.bgColor}
+									iconColor={item.iconColor}
+								/>
+							</ul>
+						))}
+					</div>
+
+					<h3 className='text-2xl font-semibold mx-auto py-5 xl:py-8 xl:w-4/5 2xl:w-5/6'>
+						Accomodation Zone
+					</h3>
+
+					<div className='flex flex-wrap col-span-5 gap-10 xl:gap-10 2xl:gap-20 items-center justify-center mx-auto py-5 xl:w-4/5 2xl:w-5/6'>
+						{categories.map((item) => (
+							<ul key={item.label}>
+								<CategoryInput
+									handleChange={handleCheckboxZoneChange}
+									label={item.label}
+									icon={item.icon}
+									id={item.label}
+									name={item.label}
+									value={item.label}
+								/>
+							</ul>
+						))}
+					</div>
+
+					{/* MAP */}
+					<div className='mx-auto py-5 xl:py-8 xl:w-4/5 2xl:w-5/6'>
+						<h3 className='text-2xl font-semibold'>Adventure location</h3>
+						<div className='flex flex-col items-center py-10'>
+							<Map />
+						</div>
+					</div>
+
+					{/* BUTTONS */}
+					<div className='flex flex-col lg:flex-row items-center justify-evenly gap-4 lg:gap-20 mx-auto py-10 w-full'>
+						<div className='w-full lg:w-2/5 xl:w-2/6'>
+							<Button label='Back' card outline onClick={() => navigate('/admindash')} />
+						</div>
+						<div className='w-full lg:w-2/5 xl:w-2/6'>
+							<Button label='Create' card type='submit' />
+						</div>
+					</div>
+				</form>
+			</div>
+		</Container>
+	)
 }
 
 export default AddAccomodation

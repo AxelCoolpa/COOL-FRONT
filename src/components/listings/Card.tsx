02d84c99f6cb667e2f9e6f33a0ced7611dee7d5c
MrTban <esteman214@gmail.com>
2023-07-12 16:43:40 -0500

    algunas cositas!!

--- src/components/listings/Card.tsx
+++ src/components/listings/Card.tsx
@@ -1,10 +1,15 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-
-import { deleteAdventure } from '../../features/deleteAdventureSlice'
-
 import { useLocation, useNavigate } from 'react-router'
+
 import { motion, useAnimation } from 'framer-motion'
+
+import { useCurrentUser } from '../../hooks/useCurrentUser'
+
+import { deleteActivity } from '../../features/deleteActivitySlice'
+import { deleteAdventure } from '../../features/deleteAdventureSlice'
+import { deleteAccomodation } from '../../features/deleteAccomodationSlice'
 
 import { EnumActivity, EnumDestination, EnumRoom } from '../../types'
 
@@ -34,10 +39,18 @@ const MainCard: React.FC<ListingCardProps> = ({ data }) => {
 	const dispatch = useDispatch()
 	const location = useLocation()
 
-	const destinationID = data?._id
+	const { currentUser } = useCurrentUser()
 
-	const onDelete = async () => {
-		await dispatch(deleteAdventure(destinationID))
+	const onDisableProvider = async () => {
+		if (currentUser?.profileProvider?.relatedChannel === 'Travel') {
+			await dispatch(deleteActivity(data?._id))
+		} else if (currentUser?.profileProvider?.relatedChannel === 'Accommodation') {
+			await dispatch(deleteAccomodation(data?._id))
+		}
+	}
+
+	const onDisableAdventure = async () => {
+		await dispatch(deleteAdventure(data?._id))
 	}
 
 	useEffect(() => {
@@ -81,7 +94,12 @@ const MainCard: React.FC<ListingCardProps> = ({ data }) => {
 														onClick={() => navigate(`/provider/update/${data?._id}`)}
 														className='cursor-pointer'
 													>
-														Edit Activity
+														{currentUser?.profileProvider?.relatedChannel === 'Travel'
+															? 'Edit activity'
+															: currentUser?.profileProvider?.relatedChannel ===
+															  'Accomodation'
+															? 'Edit room'
+															: 'Edit service'}
 													</p>
 												</div>
 											</div>
@@ -89,23 +107,98 @@ const MainCard: React.FC<ListingCardProps> = ({ data }) => {
 											<div className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap'>
 												<div className='flex gap-5 items-center '>
 													<EnableButton />
-													<p className='cursor-pointer'>Enable Activity</p>
+													<p className='cursor-pointer'>
+														{currentUser?.profileProvider?.relatedChannel === 'Travel'
+															? 'Enable activity'
+															: currentUser?.profileProvider?.relatedChannel ===
+															  'Accomodation'
+															? 'Enable room'
+															: 'Enable service'}
+													</p>
 												</div>
 											</div>
 										)}
 										{data?.itDeleted === false ? (
 											<div className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap text-blueGray-700'>
 												<div className='flex gap-5 items-center '>
-													<DisableButton onClick={onDelete} />
-													<p onClick={onDelete} className='cursor-pointer'>
-														Disable Activity
+													<DisableButton onClick={onDisableProvider} />
+													<p onClick={onDisableProvider} className='cursor-pointer'>
+														{currentUser?.profileProvider?.relatedChannel === 'Travel'
+															? 'Disable activity'
+															: currentUser?.profileProvider?.relatedChannel ===
+															  'Accomodation'
+															? 'Disable room'
+															: 'Disable service'}
 													</p>
 												</div>
 											</div>
 										) : (
 											<div className='flex gap-5 items-center '>
 												<DeleteButton />
-												<p className='cursor-pointer'>Delete Activity</p>
+												<p className='cursor-pointer'>
+													{currentUser?.profileProvider?.relatedChannel === 'Travel'
+														? 'Delete activity'
+														: currentUser?.profileProvider?.relatedChannel ===
+														  'Accomodation'
+														? 'Delete room'
+														: 'Delete service'}
+												</p>
+											</div>
+										)}
+									</div>
+								</div>
+							}
+							classNames={'py-2 top-4 right-0 w-max'}
+						/>
+					) : location.pathname === '/admindash' ? (
+						<Dropdown
+							button={<MoreOptionsButton />}
+							children={
+								<div className='flex h-48 w-56 flex-col justify-start rounded-[20px] bg-white bg-cover bg-no-repeat shadow-xl'>
+									<div className='mt-3 ml-4'>
+										<div className='flex flex-col gap-2'>
+											<p className='text-sm font-bold cursor-default'>Options</p>
+										</div>
+									</div>
+									<div className='mt-3 mx-4 flex flex-col gap-5'>
+										<div className='h-px w-full bg-gray-200' />
+										{data?.itDeleted === false ? (
+											<div className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap'>
+												<div className='flex gap-5 items-center '>
+													<EditButon
+														onClick={() => navigate(`/admindash/update/${data?._id}`)}
+													/>
+													<p
+														onClick={() => navigate(`/admindash/update/${data?._id}`)}
+														className='cursor-pointer'
+													>
+														Edit destination
+													</p>
+												</div>
+											</div>
+										) : (
+											<div className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap'>
+												<div className='flex gap-5 items-center '>
+													<EnableButton />
+													<p className='cursor-pointer'>Enable destination</p>
+												</div>
+											</div>
+										)}
+										{data?.itDeleted === false ? (
+											<div className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap text-blueGray-700'>
+												<div className='flex gap-5 items-center '>
+													<DisableButton onClick={onDisableAdventure} />
+													<p onClick={onDisableAdventure} className='cursor-pointer'>
+														Disable destination
+													</p>
+												</div>
+											</div>
+										) : (
+											<div className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap text-blueGray-700'>
+												<div className='flex gap-5 items-center '>
+													<DeleteButton />
+													<p className='cursor-pointer'>Delete destination</p>
+												</div>
 											</div>
 										)}
 									</div>

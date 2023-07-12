68ade120f43f437622ded1c2ba9380e45ea2b395
MrTban <esteman214@gmail.com>
2023-07-12 16:43:23 -0500

    Add Slect Destinations and more

--- src/components/sections/AddAccomodation.tsx
+++ src/components/sections/AddAccomodation.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import Select from 'react-select'
 
 import { useCurrentUser } from '../../hooks/useCurrentUser'
 import { useMaps } from '../../hooks/useMaps'
@@ -15,6 +16,8 @@ import {
 	createAccomodation,
 	createAccomodationFormData,
 } from '../../features/createAccomodationSlice'
+import { selectDestinations } from '../../features/destinationSlice'
+import { Destination } from './AddActivity'
 
 import AccomodationForm from '../forms/AccomodationForm'
 import CategoryInput from '../inputs/CategoryInput'
@@ -29,6 +32,10 @@ const AddAccomodation = () => {
 	const navigate = useNavigate()
 
 	const { currentUserId } = useCurrentUser()
+	const destinationsList = useSelector(selectDestinations)
+
+	const [destinations, setDestinations] = useState<Destination[]>([])
+	const [selectedDestination, setSelectedDestination] = useState<Destination | null>(null)
 
 	const [formData, setFormData] = useState<createAccomodationFormData>({
 		name: '',
@@ -44,6 +51,7 @@ const AddAccomodation = () => {
 		startDate: '',
 		endDate: '',
 		price: 0,
+		idDestination: '',
 	})
 
 	console.log(formData)
@@ -90,6 +98,14 @@ const AddAccomodation = () => {
 		}))
 	}
 
+	const handleDestinationChange = (selectedOption: any) => {
+		setSelectedDestination(selectedOption)
+		setFormData((prevFormData) => ({
+			...prevFormData,
+			idDestination: selectedOption?._id,
+		}))
+	}
+
 	const handleFilesSelected = (files: File[]) => {
 		const updatedGallery = [...formData.images, ...files]
 		setFormData((prevFormData) => ({
@@ -115,6 +131,7 @@ const AddAccomodation = () => {
 			startDate,
 			endDate,
 			price,
+			idDestination,
 		} = formData
 
 		if (
@@ -147,6 +164,7 @@ const AddAccomodation = () => {
 		data.append('startDate', startDate)
 		data.append('endDate', endDate)
 		data.append('price', price)
+		data.append('idDestination', idDestination)
 
 		for (let i = 0; i < formData.images.length; i++) {
 			data.append('images', formData.images[i])
@@ -161,7 +179,7 @@ const AddAccomodation = () => {
 		})
 
 		try {
-			dispatch(createAccomodation(formData, currentUserId))
+			dispatch(createAccomodation(data, currentUserId))
 			setFormData({
 				name: '',
 				description: '',
@@ -176,13 +194,20 @@ const AddAccomodation = () => {
 				startDate: '',
 				endDate: '',
 				price: 0,
+				idDestination: '',
 			})
+			setSelectedDestination(null)
+			setSearchValue('')
 		} catch (error: any) {
 			toast.error('Error al enviar el formulario:', error)
 		}
 	}
 
 	const { handleSearch, mapUrl, searchValue, setSearchValue } = useMaps(formData)
+
+	useEffect(() => {
+		setDestinations(destinationsList)
+	}, [destinationsList])
 
 	return (
 		<Container>
@@ -192,6 +217,55 @@ const AddAccomodation = () => {
 					{/* IMAGE */}
 					<div className='mx-auto py-5 xl:py-8 w-full xl:w-4/5 2xl:w-5/6'>
 						<DropZone onFilesSelected={handleFilesSelected} />
+					</div>
+
+					{/* SELECT DESTINATION */}
+					<h3 className='text-2xl font-semibold mx-auto py-5 xl:py-8 xl:w-4/5 2xl:w-5/6'>
+						Select destination
+					</h3>
+					<div className='mx-auto py-5 w-full md:4/5 xl:w-4/5 2xl:w-5/6'>
+						<Select
+							options={destinations}
+							value={selectedDestination}
+							onChange={handleDestinationChange}
+							placeholder='Select destination'
+							isClearable
+							formatOptionLabel={(option: any) => (
+								<div className='flex flex-row items-center gap-3'>
+									<div>
+										<span className='text-neutral-500'>{option.title}</span>
+									</div>
+								</div>
+							)}
+							classNames={{
+								control: () => 'p-3 border-2',
+								input: () => 'text-lg',
+								option: () => 'text-lg',
+							}}
+							styles={{
+								control: (provided: any) => ({
+									...provided,
+									width: '100%',
+									height: '20px',
+									minHeight: '60px',
+									borderRadius: '10px',
+								}),
+								singleValue: (provided: any) => ({
+									...provided,
+									display: 'flex',
+									alignItems: 'center',
+								}),
+							}}
+							theme={(theme) => ({
+								...theme,
+								borderRadius: 6,
+								colors: {
+									...theme.colors,
+									primary: 'white',
+									primary25: '#ce452a60',
+								},
+							})}
+						/>
 					</div>
 
 					{/* FORM */}
@@ -265,7 +339,7 @@ const AddAccomodation = () => {
 					{/* BUTTONS */}
 					<div className='flex flex-col lg:flex-row items-center justify-evenly gap-4 lg:gap-20 mx-auto py-10 w-full'>
 						<div className='w-full lg:w-2/5 xl:w-2/6'>
-							<Button label='Back' card outline onClick={() => navigate('/admindash')} />
+							<Button label='Back' card outline onClick={() => navigate('/provider')} />
 						</div>
 						<div className='w-full lg:w-2/5 xl:w-2/6'>
 							<Button label='Create' card onClick={handleSubmit} />

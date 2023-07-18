5df0a869495ca831a375d94c21e1666b6967e17c
Martin Iribas <martin.iribass@gmail.com>
2023-07-17 23:05:03 -0300

    feat(pages): pre filter

--- src/pages/LoginPreFilter/PreFilter.tsx
+++ src/pages/LoginPreFilter/PreFilter.tsx
@@ -34,22 +34,20 @@ const PreFilter: React.FC = () => {
       transition={{ duration: 0.6 }}
     >
       <div className="flex flex-col items-center justify-center h-screen m-8">
-        <img src={logo} alt="Cool-LOGO" className="flex w-[17vw] ml-36 mb-8 " />
-        <div>
-          <h1>Traveling is opening your heart to the unknown.</h1>
-        </div>
-        <div>
-          <p>Choose your favorite place</p>
-        </div>
+        <img src={logo} alt="Cool-LOGO" className="flex w-[12vw] justify-center ml-20 mb-3 " />
         <form className="w-full max-w-md p-6 ml-20" onSubmit={handleSubmit}>
-          <div className="text-2xl mb-6"></div>
+        <div className='text-2xl mb-6'>
+        <motion.h2 className='font-poppy text-3xl'>
+          Traveling is opening your heart to the unknown.
+        </motion.h2>
+        </div>
 
-          <div className="mb-4">
+          <div className="mb-6">
             <input
               type="text"
               id="destination"
               name="destination"
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 bg-gray-200 placeholder-gray-500 placeholder-opacity-75 font-poppy"
+              className="w-[21vw] py-2 px-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 bg-gray-200 placeholder-gray-500 placeholder-opacity-75 font-poppy"
               value={inputValue.destination}
               onChange={handleChange}
               required
@@ -57,24 +55,11 @@ const PreFilter: React.FC = () => {
             />
           </div>
 
-          <div className="mb-4">
-            <input
-              type="date"
-              id="date"
-              name="date"
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 bg-gray-200 placeholder-gray-500 placeholder-opacity-75 font-poppy"
-              value={inputValue.date}
-              onChange={handleChange}
-              required
-              placeholder="Choose the date"
-            />
-          </div>
-
-          <div className="flex flex-col items-center">
+          <div className="flex">
             <Link to={'guests'}>
             <button
               type="submit"
-              className="bg-orange-700 w-[10vw] hover:bg-orange-600 text-white font-medium py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-2"
+              className="bg-orange-700 w-[21vw] hover:bg-orange-600 text-white font-medium py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-2"
               >
               Go discover!
             </button>

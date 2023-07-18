ff9ef3c97eda37cc0d9360bda86dd0f1730dff88
Martin Iribas <martin.iribass@gmail.com>
2023-07-17 23:06:04 -0300

    feat(pages): step tres

--- src/pages/LoginPreFilter/PreFilterGuests.tsx
+++ src/pages/LoginPreFilter/PreFilterGuests.tsx
@@ -1,5 +1,8 @@
 import { motion } from "framer-motion";
 import logo from "../../assets/cool.png";
+import CounterInput from "../../components/userFilter/CounterInput";
+import Calendar from "../../components/userFilter/Calendar";
+import { Link } from "react-router-dom";
 
 
 const PreFilterGuests = () => {
@@ -12,12 +15,64 @@ const PreFilterGuests = () => {
       transition={{ duration: 0.6 }}
     >
         <div className="flex flex-col items-center justify-center h-screen m-8">
-        <img src={logo} alt="Cool-LOGO" className="flex w-[17vw] ml-36 mb-8 " />
-
+        <img src={logo} alt="Cool-LOGO" className="flex w-[12vw] justify-center ml-20 mb-3 " />
         
-            <div>
-                <h1>Select Guests</h1>
+            <div
+             style={{
+                fontWeight: "bold",
+                textAlign: 'left',
+                color: 'gray',
+                display: 'flex',
+                marginBottom: '1vw',
+                marginTop: '1vw',
+                justifyContent: 'flex-start'
+              }}
+            >
+                <label>Select Guests</label>
             </div>
+
+            <div
+             style={{
+                fontWeight: "bold",
+                fontSize:'small',
+                marginBottom: '1vw'
+              }}
+            >
+                <CounterInput />
+            </div>
+
+            <div
+             style={{
+                fontWeight: "bold",
+                textAlign: 'left',
+                marginBottom: '1vw',
+                color: 'gray',
+                display: 'flex',
+                justifyContent: 'left'
+              }}
+            >
+                <label>Select Date Range:</label>
+            </div>
+
+            <div
+             style={{
+                fontWeight: "bold",
+                fontSmooth: "black",
+                marginBottom: '1vw',
+              }}
+            >
+                <Calendar />
+            </div>
+            <div className="flex">
+            <Link to={'../discover'}>
+            <button
+              className="bg-orange-700 w-[21vw] hover:bg-orange-600 text-white font-medium py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-2"
+              >
+              Go discover!
+            </button>
+                </Link>            
+
+          </div>
         </div>
     </motion.div>
         </>

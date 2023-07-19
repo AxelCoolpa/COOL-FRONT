5c2264b468a16662c7a706cb243c0d0a5c289070
EmiB11 <emibarberis26@gmail.com>
2023-07-19 13:48:00 -0300

    estilos input y boton search bookingBar

--- src/components/BookingBar.tsx
+++ src/components/BookingBar.tsx
@@ -1,4 +1,4 @@
- import { useState } from 'react';
+import { useState } from 'react';
 import Input from './inputs/Input'
 import { FilterForm, filterActivities } from '../features/activitiesSlice';
 import { useDispatch } from 'react-redux';
@@ -12,44 +12,47 @@ const BookingBar = () => {
 		maxPeople: ''
 	});
 
-	const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setFormData({
-		...formData,
-	[e.target.id]: e.target.value
+			...formData,
+			[e.target.id]: e.target.value
 		});
 	};
-	
-	const handleSearch = (e:React.FormEvent) => {
-        e.preventDefault()
+
+	const handleSearch = (e: React.FormEvent) => {
+		e.preventDefault()
 		console.log(formData)
-       dispatch(filterActivities(formData))
+		dispatch(filterActivities(formData))
 	}
 
 	return (
 		<div className='absolute hidden xl:flex justify-between items-center w-11/12 p-5 bg-white shadow-black/30 shadow-lg -bottom-16 rounded-lg px-10'>
-			<form onSubmit={handleSearch}>
-			<div className='flex flex-col  justify-center gap-2'>
-				<span className='font-bold'>Location</span>
-				<Input id='location' placeholder='Location' booking value={formData.location} handleChange={handleInputChange} />
-			</div>
-			<div className='flex flex-col  justify-center gap-2'>
-				<span className='font-bold'>Start date</span>
-				<Input id='startDate' type="date" placeholder='---/-----/--' booking value={formData.startDate} handleChange={handleInputChange} />
-			</div>
-			<div className='flex flex-col  justify-center gap-2'>
-				<span className='font-bold'>Ending date</span>
-				<Input id='endDate' type="date" placeholder='---/-----/--' booking value={formData.endDate} handleChange={handleInputChange} />
-			</div>
-			<div className='flex flex-col  justify-center  gap-2'>
-				<span className='font-bold'>Peoples</span>
-				<Input id='peoples' placeholder='1 adult' booking value={formData.maxPeople} handleChange={handleInputChange} />
-			</div>
-			<div className='bg-[#CE452A] w-[147px] gap-2 text-white rounded-lg text-center cursor-pointer'>
-				<button className='font-bold p-4' type="submit">
-					Search
-				</button>
-			</div>
-		</form>
+			<form onSubmit={handleSearch} className="flex flex-col xl:flex-row">
+				
+					<div className='flex flex-col'>
+						<span className='font-bold'>Location</span>
+						<Input id='location' placeholder='Location' booking value={formData.location} handleChange={handleInputChange} />
+					</div>
+					<div className='flex flex-col'>
+						<span className='font-bold'>Start date</span>
+						<Input id='startDate' type="date" placeholder='---/-----/--' booking value={formData.startDate} handleChange={handleInputChange} />
+					</div>
+				
+					<div className='flex flex-col'>
+						<span className='font-bold'>Ending date</span>
+						<Input id='endDate' type="date" placeholder='---/-----/--' booking value={formData.endDate} handleChange={handleInputChange} />
+					</div>
+					<div className='flex flex-col'>
+						<span className='font-bold'>Peoples</span>
+						<Input id='peoples' placeholder='1 adult' booking value={formData.maxPeople} handleChange={handleInputChange} />
+					</div>
+				
+				<div className='mt-6'>
+					<button className='bg-[#CE452A] w-[147px] text-white rounded-lg p-4 font-bold text-center cursor-pointer' type="submit">
+						Search
+					</button>
+				</div>
+			</form>
 		</div>
 	)
 }

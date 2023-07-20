c12429f9e6085b92fb91f2b8fc9d4ce9e07903c2
Martin Iribas <martin.iribass@gmail.com>
2023-07-20 19:12:56 -0300

    feat(listings): refactCards

--- src/components/listings/ListingCardnew.tsx
+++ src/components/listings/ListingCardnew.tsx
@@ -10,6 +10,8 @@ import { EnumData } from "../../types";
 
 import HeartButton from "../buttons/HeartButton";
 import { useMediaQuery } from "@mui/material";
+import ShortText from "../bookingbarfilter/ShortText";
+import { Button } from "@material-tailwind/react";
 
 interface ListingCardProps {
   data: EnumData | undefined;
@@ -41,33 +43,42 @@ const ListingCardnew: React.FC<ListingCardProps> = ({ data }) => {
   const isSmallScreen = useMediaQuery("(max-width: 600px)");
 
   const containerStyle: React.CSSProperties = {
-    display: "grid",
+    display: "flex",
     gridTemplateColumns: isSmallScreen ? "1fr" : "1fr 1fr",
-    margin: "5px",
-    justifyContent: 'space-between'
+    margin: "1px",
+    justifyContent: "space-between",
+    position: "relative", // Agregar posición relativa
   };
-
   const imageStyle: React.CSSProperties = {
-    margin: '0',
-    borderRadius: '10px',
-    width: '40%'
+    borderRadius: "10px",
   };
-  const gridStyle: React.CSSProperties = {
-    width: "20vw",
+  const imageContainerStyle: React.CSSProperties = {
+    margin: "-1vw",
+    borderRadius: "10px",
+    display: 'flex',
+    position: 'relative',
+    width: "30%",
   };
   const gridStyleRight: React.CSSProperties = {
-    marginTop: "-1vw",
-    marginRight: "0",
     flexDirection: "column",
-    justifyContent: "space-between",
-    position: 'relative',
-    width: "34vw",
+    top: "1px", 
+    left: "2vw",
+    position: "relative",
+    marginRight: '3vw'
   };
   const heartStyle: React.CSSProperties = {
-    marginRight: '1px',
-    marginTop: '1px',
-    display: "flex",
-    position: 'relative'
+    top: "-10px", // Ajustar posición desde la parte superior
+    right: "-10px", // Ajustar posición desde la parte derecha
+    display: "grid",
+    position: "absolute",
+    justifyContent: "space-between",
+  };
+  const priceStyle: React.CSSProperties = {
+    bottom: "-15px", // Ajustar posición desde la parte superior
+    right: "1px", // Ajustar posición desde la parte derecha
+    display: "grid",
+    position: "absolute",
+    justifyContent: "space-between",
   };
 
   return (
@@ -77,35 +88,39 @@ const ListingCardnew: React.FC<ListingCardProps> = ({ data }) => {
       exit={{ y: -10, opacity: 0 }}
       transition={{ duration: 0.4 }}
     >
-      <div style={containerStyle}>
-        <div style={gridStyle}>
-          <img
-            src={data?.galleryImage}
-            style={imageStyle}
-          />
+      <div style={containerStyle} >
+        <div style={imageContainerStyle}>
+          <img src={data?.galleryImage} style={imageStyle} />
         </div>
 
+
         <div style={gridStyleRight}>
-          <div style={heartStyle}>
-            <HeartButton size={25} />
-          </div>
           <div
             onClick={() => navigate(`/details/${data?._id}`)}
-            className="grid items-center justify-between px-3 pt-4 cursor-pointer"
           >
             <h4 className="grid justify-between text-base font-semibold">
               {data?.title}
             </h4>
           </div>
 
-          <div className="grid justify-between text-white font-bold text-lg py-2">
-            <button
-              onClick={() => alert("Call to Action Aquí")}
-              className="bg-GreenCooL w-full mx-6 rounded-lg py-1 hover:bg-opacity-90"
-            >
-              {data?.individualPrice}$
-            </button>
+        <div>
+          <ShortText text={data?.description} maxLength={60}/>
+        </div>
           </div>
+        
+        <div style={heartStyle}>
+          <HeartButton size={25} />
+        </div>
+
+        <div style={priceStyle}>
+        <Button
+							className='shadow-md hover:shadow-lg'
+							onClick={() => navigate(`/details/activity/${data?._id}`)}
+							variant='outlined'
+							size='sm'
+						>
+							view
+						</Button>
         </div>
       </div>
     </motion.div>

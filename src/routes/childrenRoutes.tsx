0205aeb94a6982ee39c4e4c7f9c61c1fa9467669
MrTban <esteman214@gmail.com>
2023-07-28 07:49:20 -0500

    .

--- src/routes/childrenRoutes.tsx
+++ src/routes/childrenRoutes.tsx
@@ -1,175 +1,175 @@
-//* USER
-import Home from '../pages/User/Home'
-import Adventure from '../pages/User/Adventure'
-import Detail from '../pages/User/Detail'
-import Packages from '../pages/User/Packages'
-import Transport from '../pages/User/Transport'
-import Accomodation from '../pages/User/Accomodation'
-import Tickets from '../pages/User/Tickets'
-import { Maps } from '../pages/User/Maps'
-import SettingsUser from '../pages/User/Settings'
-import ProfileUser from '../pages/User/Profile'
-import Notifications from '../pages/User/Notifications'
+// //* USER
+// import Home from '../pages/User/Home'
+// import Adventure from '../pages/User/Adventure'
+// import Detail from '../pages/User/Detail'
+// import Packages from '../pages/User/Packages'
+// import Transport from '../pages/User/Transport'
+// import Accomodation from '../pages/User/Accomodation'
+// import Tickets from '../pages/User/Tickets'
+// import { Maps } from '../pages/User/Maps'
+// import SettingsUser from '../pages/User/Settings'
+// import ProfileUser from '../pages/User/Profile'
+// import Notifications from '../pages/User/Notifications'
 
-//* PROVIDER
-import ShowActivities from '../pages/ProvUser/ShowActivities'
-import CreateActivity from '../pages/ProvUser/CreateActivity'
-import Update from '../pages/ProvUser/Update'
-import ProfileProvider from '../pages/ProvUser/Profile'
-import TableUser from '../pages/ProvUser/TableUser'
-import CreateAccomodation from '../pages/ProvUser/CreateAccomodation'
-import CreateLogistic from '../pages/ProvUser/CreateLogistic'
-import PPackages from '../pages/ProvUser/Packages'
+// //* PROVIDER
+// import ShowActivities from '../pages/ProvUser/ShowActivities'
+// import CreateActivity from '../pages/ProvUser/CreateActivity'
+// import Update from '../pages/ProvUser/Update'
+// import ProfileProvider from '../pages/ProvUser/Profile'
+// import TableUser from '../pages/ProvUser/TableUser'
+// import CreateAccomodation from '../pages/ProvUser/CreateAccomodation'
+// import CreateLogistic from '../pages/ProvUser/CreateLogistic'
+// import PPackages from '../pages/ProvUser/Packages'
 
-//* ADMIN
-import Dashboard from '../pages/Admin/Dashboard'
-import ShowAdventures from '../pages/Admin/ShowAdventures'
-import CreateAdventure from '../pages/Admin/CreateAdventure'
-import UpdateAdventure from '../pages/Admin/UpdateAdventure'
-import AdminTableUser from '../pages/Admin/AdminTableUser'
-import AdminProfile from '../pages/Admin/AdminProfile'
-import Settings from '../pages/Admin/Settings'
-import PruebaHome from '../pages/ProvUser/pruebaHome'
-import PreFilter from '../pages/LoginPreFilter/PreFilter'
-import PreFilterGuests from '../pages/LoginPreFilter/PreFilterGuests'
-import PreFilterDiscover from '../pages/LoginPreFilter/PreFilterDiscover'
-import BookingBarFilter from '../pages/User/BookingBarFilter'
+// //* ADMIN
+// import Dashboard from '../pages/Admin/Dashboard'
+// import ShowAdventures from '../pages/Admin/ShowAdventures'
+// import CreateAdventure from '../pages/Admin/CreateAdventure'
+// import UpdateAdventure from '../pages/Admin/UpdateAdventure'
+// import AdminTableUser from '../pages/Admin/AdminTableUser'
+// import AdminProfile from '../pages/Admin/AdminProfile'
+// import Settings from '../pages/Admin/Settings'
+// import PruebaHome from '../pages/ProvUser/pruebaHome'
+// import PreFilter from '../pages/LoginPreFilter/PreFilter'
+// import PreFilterGuests from '../pages/LoginPreFilter/PreFilterGuests'
+// import PreFilterDiscover from '../pages/LoginPreFilter/PreFilterDiscover'
+// import BookingBarFilter from '../pages/User/BookingBarFilter'
 
-export const admin = [
-	{
-		path: '',
-		element: <Dashboard />,
-	},
-	{
-		path: 'settings',
-		element: <Settings />,
-	},
-]
-export const FilterUser = [
-	{
-		path: '',
-		element: <PreFilter />,
-	},
-	{
-		path: 'guests',
-		element: <PreFilterGuests />,
-	},
-	{
-		path: 'discover',
-		element: <PreFilterDiscover />,
-	},
-]
-export const provider = [
-	{
-		path: '',
-		element: <ShowActivities />,
-	},
-	{
-		path: 'create/activity',
-		element: <CreateActivity />,
-	},
-	{
-		path: 'create/accomodation',
-		element: <CreateAccomodation />,
-	},
-	{
-		path: 'create/logistic',
-		element: <CreateLogistic />,
-	},
-	{
-		path: 'update/:id',
-		element: <Update />,
-	},
-	{
-		path: 'clients',
-		element: <TableUser />,
-	},
-	{
-		path: 'profile',
-		element: <ProfileProvider />,
-	},
-	{
-		path: 'packages',
-		element: <PPackages />,
-	},
-]
-export const PruebaAdminProvider = [
-	{
-		path: '',
-		element: <ShowAdventures />,
-	},
-	{
-		path: 'create',
-		element: <CreateAdventure />,
-	},
-	{
-		path: 'update/:id',
-		element: <UpdateAdventure />,
-	},
-	{
-		path: 'clients',
-		element: <AdminTableUser />,
-	},
-	{
-		path: 'profile',
-		element: <AdminProfile />,
-	},
-	{
-		path: 'home',
-		element: <PruebaHome />,
-	},
-]
-export const user = [
-	{
-		path: '',
-		element: <Home />,
-	},
-	{
-		path: 'adventures',
-		element: <Adventure />,
-	},
-	{
-		path: 'packages',
-		element: <Packages />,
-	},
-	{
-		path: 'transport',
-		element: <Transport />,
-	},
-	{
-		path: 'accomodation',
-		element: <Accomodation />,
-	},
-	{
-		path: 'tickets',
-		element: <Tickets />,
-	},
-	{
-		path: 'maps',
-		element: <Maps />,
-	},
-	{
-		path: 'search',
-		element: <BookingBarFilter />,
-	},
-	{
-		path: 'create',
-		element: <CreateAdventure />,
-	},
-	{
-		path: 'details/:type/:id',
-		element: <Detail />,
-	},
-	{
-		path: '/profile',
-		element: <SettingsUser />,
-	},
-	{
-		path: 'profile/:id',
-		element: <ProfileUser />,
-	},
-	{
-		path: 'notifications/:id',
-		element: <Notifications />,
-	},
-]
+// export const admin = [
+// 	{
+// 		path: '',
+// 		element: <Dashboard />,
+// 	},
+// 	{
+// 		path: 'settings',
+// 		element: <Settings />,
+// 	},
+// ]
+// export const FilterUser = [
+// 	{
+// 		path: '',
+// 		element: <PreFilter />,
+// 	},
+// 	{
+// 		path: 'guests',
+// 		element: <PreFilterGuests />,
+// 	},
+// 	{
+// 		path: 'discover',
+// 		element: <PreFilterDiscover />,
+// 	},
+// ]
+// export const provider = [
+// 	{
+// 		path: '',
+// 		element: <ShowActivities />,
+// 	},
+// 	{
+// 		path: 'create/activity',
+// 		element: <CreateActivity />,
+// 	},
+// 	{
+// 		path: 'create/accomodation',
+// 		element: <CreateAccomodation />,
+// 	},
+// 	{
+// 		path: 'create/logistic',
+// 		element: <CreateLogistic />,
+// 	},
+// 	{
+// 		path: 'update/:id',
+// 		element: <Update />,
+// 	},
+// 	{
+// 		path: 'clients',
+// 		element: <TableUser />,
+// 	},
+// 	{
+// 		path: 'profile',
+// 		element: <ProfileProvider />,
+// 	},
+// 	{
+// 		path: 'packages',
+// 		element: <PPackages />,
+// 	},
+// ]
+// export const PruebaAdminProvider = [
+// 	{
+// 		path: '',
+// 		element: <ShowAdventures />,
+// 	},
+// 	{
+// 		path: 'create',
+// 		element: <CreateAdventure />,
+// 	},
+// 	{
+// 		path: 'update/:id',
+// 		element: <UpdateAdventure />,
+// 	},
+// 	{
+// 		path: 'clients',
+// 		element: <AdminTableUser />,
+// 	},
+// 	{
+// 		path: 'profile',
+// 		element: <AdminProfile />,
+// 	},
+// 	{
+// 		path: 'home',
+// 		element: <PruebaHome />,
+// 	},
+// ]
+// export const user = [
+// 	{
+// 		path: '',
+// 		element: <Home />,
+// 	},
+// 	{
+// 		path: 'adventures',
+// 		element: <Adventure />,
+// 	},
+// 	{
+// 		path: 'packages',
+// 		element: <Packages />,
+// 	},
+// 	{
+// 		path: 'transport',
+// 		element: <Transport />,
+// 	},
+// 	{
+// 		path: 'accomodation',
+// 		element: <Accomodation />,
+// 	},
+// 	{
+// 		path: 'tickets',
+// 		element: <Tickets />,
+// 	},
+// 	{
+// 		path: 'maps',
+// 		element: <Maps />,
+// 	},
+// 	{
+// 		path: 'search',
+// 		element: <BookingBarFilter />,
+// 	},
+// 	{
+// 		path: 'create',
+// 		element: <CreateAdventure />,
+// 	},
+// 	{
+// 		path: 'details/:type/:id',
+// 		element: <Detail />,
+// 	},
+// 	{
+// 		path: '/profile',
+// 		element: <SettingsUser />,
+// 	},
+// 	{
+// 		path: 'profile/:id',
+// 		element: <ProfileUser />,
+// 	},
+// 	{
+// 		path: 'notifications/:id',
+// 		element: <Notifications />,
+// 	},
+// ]
--- src/routes/createRouter.tsx
+++ src/routes/createRouter.tsx
@@ -1,77 +1,76 @@
-import { createBrowserRouter } from 'react-router-dom'
-import { User } from '../layouts/User'
-import { Admin } from '../layouts/Admin'
-import { Provider } from '../layouts/Provider'
-import { ProvRegister } from '../layouts/ProvRegister'
-import PrivateRoute from './PrivateRoute'
+// import { createBrowserRouter } from 'react-router-dom'
+// import { User } from '../layouts/User'
+// import { Admin } from '../layouts/Admin'
+// import { Provider } from '../layouts/Provider'
+// import { ProvRegister } from '../layouts/ProvRegister'
+// import PrivateRoute from './PrivateRoute'
 
-import Registerprovideer from '../pages/ProvUser/Register-provideer'
-import LoginPage from '../pages/LoginPage'
-import RegisterPage from '../pages/RegisterPage'
-import { admin, provider, user, PruebaAdminProvider, FilterUser } from './childrenRoutes'
-import { AdminDash } from '../layouts/AdminDash'
-import FilterUserLayout from '../layouts/FilterUserLayout'
+// import Registerprovideer from '../pages/ProvUser/Register-provideer'
+// import LoginPage from '../pages/LoginPage'
+// import RegisterPage from '../pages/RegisterPage'
+// import { admin, provider, user, PruebaAdminProvider, FilterUser } from './childrenRoutes'
+// import { AdminDash } from '../layouts/AdminDash'
+// import FilterUserLayout from '../layouts/FilterUserLayout'
 
+// export const router = createBrowserRouter([
+// 	{
+// 		path: '/admin',
+// 		element: (
+// 			<PrivateRoute>
+// 				<Admin />
+// 			</PrivateRoute>
+// 		),
+// 		children: admin,
+// 	},
+// 	{
+// 		path: '/',
+// 		element: (
+// 			<PrivateRoute>
+// 				<User />
+// 			</PrivateRoute>
+// 		),
+// 		children: user,
+// 	},
+// 	{
+// 		path: '/provider',
+// 		element: (
+// 			<PrivateRoute>
+// 				<Provider />
+// 			</PrivateRoute>
+// 		),
+// 		children: provider,
+// 	},
+// 	{
+// 		path: '/admindash',
+// 		element: (
+// 			<PrivateRoute>
+// 				<AdminDash />
+// 			</PrivateRoute>
+// 		),
+// 		children: PruebaAdminProvider
+// 	},
+// 	{
+// 		path: '/proveedor-registro',
+// 		element: <ProvRegister />,
 
-export const router = createBrowserRouter([
-	{
-		path: '/admin',
-		element: (
-			<PrivateRoute>
-				<Admin />
-			</PrivateRoute>
-		),
-		children: admin,
-	},
-	{
-		path: '/',
-		element: (
-			<PrivateRoute>
-				<User />
-			</PrivateRoute>
-		),
-		children: user,
-	},
-	{
-		path: '/provider',
-		element: (
-			<PrivateRoute>
-				<Provider />
-			</PrivateRoute>
-		),
-		children: provider,
-	},
-	{
-		path: '/admindash',
-		element: (
-			<PrivateRoute>
-				<AdminDash />
-			</PrivateRoute>
-		),
-		children: PruebaAdminProvider
-	},
-	{
-		path: '/proveedor-registro',
-		element: <ProvRegister />,
-
-		children: [
-			{
-				path: '',
-				element: <Registerprovideer />,
-			},
-		],
-	},
-	{
-		path: '/login',
-		element: <LoginPage />,
-	},
-	{
-		path: '/register',
-		element: <RegisterPage />,
-	},
-	{
-		path: '/filter',
-		element: <FilterUserLayout />,
-		children: FilterUser
-	},
-])
+// 		children: [
+// 			{
+// 				path: '',
+// 				element: <Registerprovideer />,
+// 			},
+// 		],
+// 	},
+// 	{
+// 		path: '/login',
+// 		element: <LoginPage />,
+// 	},
+// 	{
+// 		path: '/register',
+// 		element: <RegisterPage />,
+// 	},
+// 	{
+// 		path: '/filter',
+// 		element: <FilterUserLayout />,
+// 		children: FilterUser
+// 	},
+// ])
